2e551ec67ea36502999b12460181fe1bedf10abc
Matt Palmer <devnull@localhost>
2011-01-01 14:34:05 +0000

    Added some java doc.

--- src/net/domesdaybook/automata/DeepCopy.java
+++ src/net/domesdaybook/automata/DeepCopy.java
@@ -8,10 +8,24 @@ package net.domesdaybook.automata;
 import java.util.Map;
 
 /**
+ * DeepCopy
  *
+ * An interface for deep copying of objects.
+ * Each object implementing this  interface must return a deep copy of itself
+ * and any child objects that are not immutable.
+ * It requires an initially empty map of old to new objects to be passed in.
+ * This is forwarded to other child objects implementing DeepCopy, in order that
+ * only one copy of the same object is ever created.
+
  * @author matt
  */
 public interface DeepCopy {
 
+    /**
+     * deepCopy returns a deep copy of the object implementing this interface.
+     *
+     * @param oldToNewObjects a map of old objects to their copies.
+     * @return DeepCopy a deep copy of the object implementing this interface.
+     */
     DeepCopy deepCopy(Map<DeepCopy,DeepCopy> oldToNewObjects);
 }
