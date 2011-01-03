25ebcb36d0c5841cbbd5a7ea22b31e942ff508dc
Matt Palmer <devnull@localhost>
2011-01-03 18:54:48 +0000

    Implemented naive searcher methods.

--- src/net/domesdaybook/matcher/sequence/searcher/NaiveSearcher.java
+++ src/net/domesdaybook/matcher/sequence/searcher/NaiveSearcher.java
@@ -5,8 +5,8 @@
 
 package net.domesdaybook.matcher.sequence.searcher;
 
+import net.domesdaybook.matcher.sequence.SequenceMatcher;
 import net.domesdaybook.reader.ByteReader;
-import net.domesdaybook.matcher.sequence.CombinedSequenceMatcher;
 
 /**
  *
@@ -14,18 +14,39 @@ import net.domesdaybook.matcher.sequence.CombinedSequenceMatcher;
  */
 public final class NaiveSearcher extends SequenceMatcherSearcher {
 
-    public NaiveSearcher(CombinedSequenceMatcher sequence) {
+    public NaiveSearcher(SequenceMatcher sequence) {
         super(sequence);
     }
 
+    
     @Override
     public final long searchForwards(final ByteReader reader, final long fromPosition, final long toPosition) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        long matchPosition = -1L;
+        long searchPosition = fromPosition;
+        final long lastPosition = toPosition - matcher.length() - 1;
+        while (searchPosition <= lastPosition) {
+            if (matcher.matches(reader, searchPosition)) {
+                matchPosition = searchPosition;
+                break;
+            }
+            searchPosition++;
+        }
+        return matchPosition;
     }
+
 
     @Override
     public final long searchBackwards(final ByteReader reader, final long fromPosition, final long toPosition) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        long matchPosition = -1L;
+        long searchPosition = fromPosition - matcher.length() + 1;
+        while (searchPosition >= toPosition) {
+            if (matcher.matches(reader, searchPosition)) {
+                matchPosition = searchPosition;
+                break;
+            }
+            searchPosition--;
+        }
+        return matchPosition;
     }
 
 }
