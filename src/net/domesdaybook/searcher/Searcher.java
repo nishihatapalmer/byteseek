4e0ccd7ab168c133c08f5d7166b5ce8a79cfd092
Matt Palmer <devnull@localhost>
2011-01-03 22:55:06 +0000

    Added some javadoc

--- src/net/domesdaybook/searcher/Searcher.java
+++ src/net/domesdaybook/searcher/Searcher.java
@@ -8,15 +8,42 @@ package net.domesdaybook.searcher;
 import net.domesdaybook.reader.ByteReader;
 
 /**
+ * An interface for classes that search bytes provided by a {@link ByteReader}.
+ * Searching can be forwards or backwards.
  *
  * @author matt
  */
 public interface Searcher {
 
+    /**
+     * A constant indicating whether a search was not successful.
+     */
     public long NOT_FOUND = -1L;
 
+
+    /**
+     * Searches bytes forwards provided by a {@link ByteReader} object,
+     * forwards from the position given by fromPosition up to the position
+     * given by toPosition.
+     *
+     * @param reader       The byte reader giving access to the bytes being searched.
+     * @param fromPosition The position to search from.
+     * @param toPosition   The position to search up to.
+     * @return             The position a match was found at, or NOT_FOUND (-1).
+     */
     public long searchForwards(final ByteReader reader, final long fromPosition, final long toPosition);
 
+
+    /**
+     * Searches bytes backwards provided by a {@link ByteReader} object,
+     * backwards from the position given by fromPosition up to the position
+     * given by toPosition.
+     *
+     * @param reader       The byte reader giving access to the bytes being searched.
+     * @param fromPosition The position to search from.
+     * @param toPosition   The position to search back to.
+     * @return             The position a match was found at, or NOT_FOUND (-1).
+     */
     public long searchBackwards(final ByteReader reader, final long fromPosition, final long toPosition);
 
 }
