64781b49e71bdf0ae635b36aa0daf9bc370c3b44
Matt Palmer <devnull@localhost>
2011-01-03 23:07:10 +0000

    Added some javadoc, made NOT_FOUND static final.

--- src/net/domesdaybook/reader/ByteReader.java
+++ src/net/domesdaybook/reader/ByteReader.java
@@ -6,7 +6,8 @@
 package net.domesdaybook.reader;
 
 /**
- *
+ * An interface for classes which can read bytes at a given position.
+ * 
  * @author matt
  */
 public interface ByteReader {
@@ -14,9 +15,9 @@ public interface ByteReader {
     /**
      * Get a byte from a position
      *
-     * @param fileIndex position of required byte 
+     * @param position the position of the byte to read.
      * @return the byte at position <code>fileIndex</code>
      */
-    public byte getByte(final long fileIndex);
+    public byte getByte(final long position);
 
 }
