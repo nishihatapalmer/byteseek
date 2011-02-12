9849e0e2685d588b82dcd83ffa95de0aafc135bd
Matt Palmer <devnull@localhost>
2011-02-12 23:23:01 +0000

    removed unnecessary import.

--- src/net/domesdaybook/expression/compiler/sequence/SequenceMatcherCompiler.java
+++ src/net/domesdaybook/expression/compiler/sequence/SequenceMatcherCompiler.java
@@ -5,7 +5,6 @@
 
 package net.domesdaybook.expression.compiler.sequence;
 
-import com.sun.xml.internal.messaging.saaj.util.ParseUtil;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
