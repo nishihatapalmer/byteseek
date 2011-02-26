42fbb85b3893b230c86da11ae2a88027166d5a85
Matt Palmer <devnull@localhost>
2011-02-26 20:51:16 +0000

    Made class final, added some javadoc.

--- src/net/domesdaybook/expression/compiler/nfa/SimpleStateBuilder.java
+++ src/net/domesdaybook/expression/compiler/nfa/SimpleStateBuilder.java
@@ -8,11 +8,19 @@ package net.domesdaybook.expression.compiler.nfa;
 import net.domesdaybook.automata.nfa.NfaSimpleState;
 
 /**
+ * Implements the StateBuilder interface to create
+ * {@link NfaSimpleState} objects.
  *
  * @author Matt Palmer
  */
-public class SimpleStateBuilder implements StateBuilder {
+public final class SimpleStateBuilder implements StateBuilder {
 
+    /**
+     * Builds an {@link NfaState} object.
+     *
+     * @param isFinal Whether the state is final or not.
+     * @return An {@link NfaSimpleState} object, implementing the NfaState interface.
+     */
     @Override
     public NfaSimpleState build(final boolean isFinal) {
         return new NfaSimpleState(isFinal);
