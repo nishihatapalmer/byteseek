4a74743189eff7a75c24012b4eea6305aca10696
Matt Palmer <devnull@localhost>
2011-02-28 00:00:20 +0000

    Added javadoc.

--- src/net/domesdaybook/matcher/sequence/SequenceMatcher.java
+++ src/net/domesdaybook/matcher/sequence/SequenceMatcher.java
@@ -9,27 +9,35 @@ import net.domesdaybook.matcher.Matcher;
 import net.domesdaybook.matcher.singlebyte.SingleByteMatcher;
 
 /**
+ * An extension to the {@link Matcher} interface to support sequences.
  *
  * @author Matt Palmer
  */
  public interface SequenceMatcher extends Matcher {
 
     /**
+     * Returns a {@link SingleByteMatcher} which matches all the bytes at
+     * the requested position in the sequence.
      *
-     * @param position the position in the byte matcher to return a dedicated byte matcher for.
-     * @return a byte matcher for a single position.
+     * @param position The position in the byte matcher to return a dedicated byte matcher for.
+     * @return A SingleByteMatcher for the position in the sequence provided.
      */
     public SingleByteMatcher getByteMatcherForPosition(final int position);
 
 
-    /*
-     * @returns Returns the length of a matching byte sequence.
+    /**
+     * Gets the length of the matching sequence.
+     *
+     * @return Returns the length of a matching byte sequence.
      */
     public int length();
 
     
-    /* @param prettyPrint whether to pretty print the regular expression with spacing.
-     * @returns Returns a string containing a regular expression of the byte matcher.
+    /**
+     * Returns a regular expression representation of the matching sequence.
+     * 
+     * @param prettyPrint whether to pretty print the regular expression with spacing.
+     * @return A string containing a regular expression of the byte matcher.
      */
     public String toRegularExpression(final boolean prettyPrint);
 
