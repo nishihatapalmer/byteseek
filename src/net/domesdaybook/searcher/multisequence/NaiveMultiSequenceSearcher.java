7f6145597ef17cfd69ed008d1ade6e73364298e4
Matt Palmer <devnull@localhost>
2011-06-27 23:53:57 +0100

    new searcher (unwritten yet)

---
+++ src/net/domesdaybook/searcher/multisequence/NaiveMultiSequenceSearcher.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.domesdaybook.searcher.multisequence;
+
+/**
+ *
+ * @author matt
+ */
+public class NaiveMultiSequenceSearcher {
+    //TODO: write me.
+}
