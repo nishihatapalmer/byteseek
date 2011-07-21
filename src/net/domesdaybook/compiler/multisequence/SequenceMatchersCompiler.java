d934952962fd4dfc0cb059232d5334239006b706
Matt Palmer <devnull@localhost>
2011-07-21 23:36:21 +0100

    Added static method to produce a list of sequence matchers from a list of byte arrays.

--- src/net/domesdaybook/compiler/multisequence/SequenceMatchersCompiler.java
+++ src/net/domesdaybook/compiler/multisequence/SequenceMatchersCompiler.java
@@ -6,11 +6,14 @@
 
 package net.domesdaybook.compiler.multisequence;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import net.domesdaybook.compiler.CompileException;
 import net.domesdaybook.compiler.Compiler;
 import net.domesdaybook.compiler.sequence.SequenceMatcherCompiler;
+import net.domesdaybook.matcher.sequence.ByteSequenceMatcher;
 import net.domesdaybook.matcher.sequence.SequenceMatcher;
 
 /**
@@ -20,9 +23,20 @@ import net.domesdaybook.matcher.sequence.SequenceMatcher;
 public final class SequenceMatchersCompiler implements Compiler<Collection<SequenceMatcher>, Collection<String>> {
 
     private static SequenceMatchersCompiler defaultCompiler;
+    
     public static Collection<SequenceMatcher> sequenceMatchersFrom(final Collection<String> expressions) throws CompileException {
         defaultCompiler = new SequenceMatchersCompiler();
         return defaultCompiler.compile(expressions);
+    }
+    
+    public static Collection<SequenceMatcher> sequenceMatchersFrom(final List<byte[]> bytes) throws CompileException {
+        defaultCompiler = new SequenceMatchersCompiler();
+        List<SequenceMatcher> matchers = new ArrayList<SequenceMatcher>(bytes.size());
+        for (final byte[] bytesToUse : bytes) {
+            final SequenceMatcher byteMatcher = new ByteSequenceMatcher(bytesToUse);
+            matchers.add(byteMatcher);
+        }     
+        return matchers;
     }
     
     
