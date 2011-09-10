8415c3e54e19ab42d730effc3ee702039a9ac383
Matt Palmer <devnull@localhost>
2011-09-10 19:26:18 +0100

    Made FileArrayProvider implement the Iterator interface, to easily iterate across a file in chunks.

--- src/net/domesdaybook/reader/FileArrayProvider.java
+++ src/net/domesdaybook/reader/FileArrayProvider.java
@@ -9,25 +9,27 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Iterator;
 
 /**
  *
  * @author matt
  */
-public class FileArrayProvider implements ArrayProvider {
+public class FileArrayProvider implements ArrayProvider, Iterable {
 
     private final static String READ_ONLY = "r";
     private final static String NULL_ARGUMENTS = "Null file passed to FileByteArrayProvider";
 
-    private final int cacheBlockSize;
+    private final static int DEFAULT_ARRAY_SIZE = 4096;
+    private final int arraySize;
     
     private final RandomAccessFile file;
     private final long length;
 
 
     /**
-     * Constructs an immutable FileReader.
-     *
+     * Constructs an immutable FileReader which defaults to an array size of 4096.
+     * 
      * @param file The file to read from.
      * @throws FileNotFoundException If the file does not exist.
      * @throws IllegalArgumentException if the file passed in is null.
@@ -38,13 +40,13 @@ public class FileArrayProvider implements ArrayProvider {
         }
         this.file = new RandomAccessFile(file, READ_ONLY);
         this.length = file.length();
-        this.cacheBlockSize = this.length < Integer.MAX_VALUE ?
-                (int) this.length : Integer.MAX_VALUE;
+        this.arraySize = DEFAULT_ARRAY_SIZE;
     }
     
 
     /**
-     * Constructs an immutable FileReader.
+     * Constructs an immutable FileReader which reads the file into arrays of
+     * the specified size.
      *
      * @param file The file to read from.
      * @throws FileNotFoundException If the file does not exist.
@@ -56,7 +58,7 @@ public class FileArrayProvider implements ArrayProvider {
         }
         this.file = new RandomAccessFile(file, READ_ONLY);
         this.length = file.length();
-        this.cacheBlockSize = arraySize;
+        this.arraySize = arraySize;
     }    
 
 
@@ -79,7 +81,7 @@ public class FileArrayProvider implements ArrayProvider {
     public Array getByteArray(final long position) throws ByteReaderException {
         if (position >= 0 && position < length) {
             try {
-                int blockSize = cacheBlockSize;
+                int blockSize = arraySize;
                 final long readPos = (position / blockSize) * blockSize;
                 final int offset = (int) (position % blockSize); 
                 
@@ -98,7 +100,32 @@ public class FileArrayProvider implements ArrayProvider {
         }
         return Array.EMPTY;
     }
+
+    @Override
+    public Iterator<Array> iterator() {
+        return new FileArrayIterator();
+    }
     
-    
+    private class FileArrayIterator implements Iterator<Array> {
+
+        private int position = 0;
+        
+        @Override
+        public boolean hasNext() {
+            return position < length;
+        }
+
+        @Override
+        public Array next() {
+            final Array array = getByteArray(position);
+            position += arraySize;
+            return array;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Cannot remove Arrays from the FileArrayIterator.");
+        }
+    }
     
 }
