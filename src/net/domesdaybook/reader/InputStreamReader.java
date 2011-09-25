5f4182d1402da2ea50e4ad5833f03fe30a341978
Matt Palmer <devnull@localhost>
2011-09-25 21:14:20 +0100

    Overrode getWindow() to detect a cache failure, where an earlier position (which must be in the cache) isn't.  Since an InputStream cannot read back to get the data again, it now throws a CacheFailureException if the needed data cannot be provided.   This is essentially a programming error in selecting a broken backing cache to use with the InputStreamReader.

--- src/net/domesdaybook/reader/InputStreamReader.java
+++ src/net/domesdaybook/reader/InputStreamReader.java
@@ -39,47 +39,60 @@ public class InputStreamReader extends AbstractReader {
         this.stream = stream;
     }    
     
- 
+    
     @Override
-    Window createWindow(final long readPos) throws ReaderException {
+    public final Window getWindow(final long position) 
+            throws IOException, CacheFailureException {
+        final Window window = super.getWindow(position);
+        if (window == null && position < streamPos && position >= 0) {
+            // No window was returned, but the position requested has already
+            // been read. This means the cache algorithm selected to use with
+            // this reader cannot return an earlier position, and being a stream,
+            // we can't rewind to read it again.  There is nothing which can be
+            // done at this point other than to throw an exception.
+            // This runtime exception flags a programming error, in selecting an
+            // innapropriate cache algorithm to use for the access needed.
+            // A reader should always be able to return a window for a valid position,
+            // setting aside genuine IO Exceptions.
+            final String message = "Cache failed to provide a window at position: %d when we have already read past this position, currently at: %d";
+            throw new CacheFailureException(String.format(message, position, streamPos));
+        }
+        return window;
+    }
+ 
+    
+    @Override
+    Window createWindow(final long readPos) throws IOException {
         Window lastWindow = null;
-        try {
-            while (readPos > streamPos && length == UNKNOWN_LENGTH) {
-                final byte[] bytes = new byte[windowSize];
-                final int totalRead = ReadUtils.readBytes(stream, bytes);
-                if (totalRead > 0) {
-                    lastWindow = new Window(bytes, streamPos, totalRead);  
-                    streamPos += totalRead;                                        
-                    cache.addWindow(lastWindow);
-                }
-                if (totalRead < windowSize) { // If we read less than the available array:
-                    length = streamPos; // then the length is whatever the streampos is now.
-                }
+        while (readPos > streamPos && length == UNKNOWN_LENGTH) {
+            final byte[] bytes = new byte[windowSize];
+            final int totalRead = ReadUtils.readBytes(stream, bytes);
+            if (totalRead > 0) {
+                lastWindow = new Window(bytes, streamPos, totalRead);  
+                streamPos += totalRead;                                        
+                cache.addWindow(lastWindow);
             }
-        } catch (IOException io) {
-            throw new ReaderException(io);
+            if (totalRead < windowSize) { // If we read less than the available array:
+                length = streamPos; // then the length is whatever the streampos is now.
+            }
         }
         return lastWindow;
     }    
 
     
     @Override
-    public long length() {
-        try {
-            while (length == UNKNOWN_LENGTH) {
-                final byte[] bytes = new byte[windowSize];
-                final int totalRead = ReadUtils.readBytes(stream, bytes);
-                if (totalRead > 0) {
-                    final Window lastWindow = new Window(bytes, streamPos, totalRead); 
-                    streamPos += totalRead;                  
-                    cache.addWindow(lastWindow);
-                }
-                if (totalRead < windowSize) { // If we read less than the available array:
-                    length = streamPos;
-                }
+    public long length() throws IOException {
+        while (length == UNKNOWN_LENGTH) {
+            final byte[] bytes = new byte[windowSize];
+            final int totalRead = ReadUtils.readBytes(stream, bytes);
+            if (totalRead > 0) {
+                final Window lastWindow = new Window(bytes, streamPos, totalRead); 
+                streamPos += totalRead;                  
+                cache.addWindow(lastWindow);
             }
-        } catch (IOException io) {
-            throw new ReaderException(io);
+            if (totalRead < windowSize) { // If we read less than the available array:
+                length = streamPos;
+            }
         }
         return length;
     }
