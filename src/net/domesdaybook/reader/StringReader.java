c16e09d0294daef8dad7948796053d7624ef140c
Matt Palmer <devnull@localhost>
2011-09-27 22:47:39 +0100

    Added new StringReader class, which stores a String as a sequence of bytes encoded either using the platform default character set encoding, or a specified one.

---
+++ src/net/domesdaybook/reader/StringReader.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright Matt Palmer 2011, All rights reserved.
+ *
+ */
+
+package net.domesdaybook.reader;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import net.domesdaybook.reader.cache.NoCache;
+
+/**
+ *
+ * @author matt
+ */
+public class StringReader extends AbstractReader {
+
+    private final byte[] string;
+    private final Charset charset;
+    
+    public StringReader(final String string) {
+        this(string, Charset.defaultCharset());
+    }
+    
+    
+    public StringReader(final String string, final Charset charset) {
+        super(string == null? 0 : string.length(), NoCache.NO_CACHE);        
+        if (string == null) {
+            throw new IllegalArgumentException("Null string passed in to StringReader.");
+        }
+        if (charset == null) {
+            throw new IllegalArgumentException("Null charset passed in to StringReader.");
+        }
+        this.string = string.getBytes(charset);
+        this.charset = charset;
+    }
+    
+    
+    @Override
+    Window createWindow(final long windowStart) throws IOException {
+        return new Window(string, 0, string.length);
+    }
+
+    
+    @Override
+    public long length() throws IOException {
+        return string.length;
+    }
+    
+    
+    public String getString() {
+        return new String(string, charset);
+    }
+    
+}
