aacc9d297fc3168d5321b1fb5ba28efb40849312
Matt Palmer <devnull@localhost>
2011-10-16 21:44:41 +0100

    Updated javadoc.

--- src/net/domesdaybook/automata/Transition.java
+++ src/net/domesdaybook/automata/Transition.java
@@ -15,8 +15,6 @@
  * 
  *  * The names of its contributors may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
- * 
- *  
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
