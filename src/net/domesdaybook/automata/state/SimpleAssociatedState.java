b0e42197f6fcd050bb8ac7cb40a12b3239e80ea3
Matt Palmer <devnull@localhost>
2011-10-17 22:49:39 +0100

    Updated javadoc

--- src/net/domesdaybook/automata/state/SimpleAssociatedState.java
+++ src/net/domesdaybook/automata/state/SimpleAssociatedState.java
@@ -16,8 +16,6 @@
  *  * The names of its contributors may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
  * 
- *  
- *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
@@ -34,7 +32,6 @@
 
 package net.domesdaybook.automata.state;
 
-import net.domesdaybook.automata.TransitionStrategy;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.IdentityHashMap;
@@ -46,138 +43,104 @@ import net.domesdaybook.automata.Transition;
 import net.domesdaybook.object.copy.DeepCopy;
 
 /**
- *
- * @param <T> 
- * @author matt
+ * An implementation of the {@link AssociatedState} interface, which also 
+ * inherits most of its implementation from the {@link SimpleState} class.
+ * Only managing the association with other objects is added.
+ * <p>
+ * This class is intentionally not final, to allow other States to inherit from it.
+ * 
+ * @param <T> The type of object this State can be associated with.
+ * @see SimpleState
+ * @see net.domesdaybook.automata.State
+ * @see net.domesdaybook.automata.AssociatedState
+ * 
+ * @author Matt Palmer
  */
-public class SimpleAssociatedState<T> implements AssociatedState {
+public class SimpleAssociatedState<T> extends SimpleState implements AssociatedState {
     
-    private List<Transition> transitions;
-    private boolean isFinal;
-    private TransitionStrategy transitionStrategy = NO_TRANSITION;
     private List<T> associations;
     
-    /**
-     * 
-     * @param isFinal
-     */
-    public SimpleAssociatedState(final boolean isFinal) {
-        this.isFinal = isFinal;
-        this.transitions = new ArrayList<Transition>();
-    }
-
     
     /**
-     * 
-     * @param other
-     */
-    public SimpleAssociatedState(final SimpleAssociatedState other) {
-        this.isFinal = other.isFinal;
-        this.transitions = new ArrayList<Transition>(other.transitions); 
-    }
-
-    
-    /**
-     * 
+     * Default constructor for the SimpleAssociatedState as a non-final State.
      */
     public SimpleAssociatedState() {
         this(State.NON_FINAL);
+    }    
+    
+    
+    /**
+     * Constructor for the SimpleAssociatedState taking a parameter controlling
+     * whether it is final or not.
+     * 
+     * @param isFinal Whether the state is final or not.
+     */
+    public SimpleAssociatedState(final boolean isFinal) {
+        super(isFinal);
+        // It is likely that most associated states will only have zero or one 
+        // object associated with them, so create an arraylist with a very small
+        // initial capacity.
+        this.associations = new ArrayList<T>(1); 
     }
 
     
-    @Override
-    public void addTransition(final Transition transition) {
-        this.transitions.add(transition);
+    /**
+     * Copy constructor for the SimpleAssociatedState from another AssociatedState
+     * 
+     * @param other The AssociatedState to construct ourselves from.
+     */
+    public SimpleAssociatedState(final AssociatedState<T> other) {
+        super(other);
+        this.associations = new ArrayList<T>(other.getAssociations());
     }
+ 
 
     
-    @Override
-    public void addAllTransitions(final List<Transition> transitions) {
-        this.transitions.addAll(transitions);
-        setBasicStrategy();
-    }
-
-    
-    @Override
-    public void removeTransition(final Transition transition) {
-        this.transitions.remove(transition);
-        setBasicStrategy();
-    }
-    
-    
-    @Override
-    public void appendNextStatesForByte(final Collection<State> states, byte value) {
-        transitionStrategy.appendDistinctStatesForByte(states, value, transitions);
-    }
-
-    
-    private void setBasicStrategy() {
-        if (transitions.isEmpty()) {
-            transitionStrategy = NO_TRANSITION;
-        } else if (transitions.size() == 1) {
-            transitionStrategy = FIRST_MATCHING_TRANSITION;
-        } else {
-            // Determining whether the state satisfies a DFA strategy is expensive
-            // so defer this decision to the optimise() method call.
-            transitionStrategy = ALL_MATCHING_TRANSITIONS;
-        }
-    }
-    
-    @Override
-    public boolean isFinal() {
-        return isFinal;
-    }
-
-    @Override
-    public List<Transition> getTransitions() {
-        return new ArrayList<Transition>(this.transitions);
-    }
-    
-    
+    /**
+     * This is a convenience method, providing the initial map to:
+     * <CODE>deepCopy(Map<DeepCopy, DeepCopy> oldToNewObjects)</CODE>
+     *
+     * @return SimpleAssociatedState a deep copy of this object.
+     * @see #deepCopy(Map<DeepCopy, DeepCopy> oldToNewObjects)
+     */
     @Override
     public SimpleAssociatedState<T> deepCopy() {
         final Map<DeepCopy, DeepCopy> oldToNewObjects = new IdentityHashMap<DeepCopy,DeepCopy>();
         return deepCopy(oldToNewObjects);
     }
+    
 
+    /**
+     * This method is inherited from the {@link DeepCopy} interface,
+     * and is redeclared here with a return type of SimpleAssociatedState 
+     * (rather than DeepCopy), to make using the method easier.
+     *
+     * @param oldToNewObjects A map of the original objects to their new deep copies.
+     * @return SimpleAssociatedState A deep copy of this SimpleAssociatedState 
+     *         and any Transitions and States reachable from this State.
+     */
     @Override
     public SimpleAssociatedState<T> deepCopy(final Map<DeepCopy, DeepCopy> oldToNewObjects) {
         SimpleAssociatedState<T> stateCopy = (SimpleAssociatedState<T>) oldToNewObjects.get(this);
         if (stateCopy == null) {
-            stateCopy = new SimpleAssociatedState<T>(this.isFinal);
+            stateCopy = new SimpleAssociatedState<T>(isFinal());
             oldToNewObjects.put(this, stateCopy);
-            for (Transition transition : transitions) {
+            for (Transition transition : getTransitions()) {
                 final Transition transitionCopy = transition.deepCopy(oldToNewObjects);
-                stateCopy.transitions.add(transitionCopy);
+                stateCopy.addTransition(transitionCopy);
             }
-            stateCopy.setTransitionStrategy(transitionStrategy.deepCopy(oldToNewObjects));
+            stateCopy.setTransitionStrategy(getTransitionStrategy().deepCopy(oldToNewObjects));
             stateCopy.setAssociations(associations); // does not deep copy associations.
         }
         return stateCopy;
     }
 
     
-    @Override
-    public void setIsFinal(final boolean isFinal) {
-        this.isFinal = isFinal;
-    }
-
-    
-    @Override
-    public void setTransitionStrategy(final TransitionStrategy strategy) {
-        this.transitionStrategy = strategy;
-    }
-
-    
-    @Override
-    public TransitionStrategy getTransitionStrategy() {
-        return transitionStrategy;
-    }
-
-    
     /**
+     * Returns the list of objects associated with this state.
+     * This list is the actual list used by the SimpleAssociatedState, not a copy.
      * 
-     * @return
+     * @return The list of objects associated with this state.
      */
     @Override
     public Collection<T> getAssociations() {
@@ -186,46 +149,37 @@ public class SimpleAssociatedState<T> implements AssociatedState {
 
     
     /**
+     * Adds an object to the list of associated objects held by this state.
      * 
-     * @param object
+     * @param object The object to add to this State.
      */
     @Override
     public void addObject(final Object object) {
-        if (associations == null) {
-            associations = new ArrayList<T>(1);
-        }
         associations.add((T) object);
     }
 
     
     /**
+     * Removes an object associated with this State.
      * 
-     * @param object
+     * @param object The object to remove from this State.
+     * @return boolean Whether the object was associated with the State.
      */
     @Override
     public boolean removeObject(final Object object) {
-        boolean result = false;
-        if (associations != null) {
-            result = associations.remove((T) object);
-            if (associations.isEmpty()) {
-                associations = null;
-            }
-        }
-        return result;
+        return associations.remove((T) object);
     }
 
     
     /**
+     * Sets a new list of associations, copying them from the collection
+     * passed in.
      * 
-     * @param associations
+     * @param associations A collection of associations to associate with this State.
      */
     @Override
     public void setAssociations(final Collection associations) {
-        if (associations == null) {
-            this.associations = null;
-        } else {
-            this.associations = new ArrayList<T>(associations);
-        }
+        this.associations = new ArrayList<T>(associations);
     }
     
    
