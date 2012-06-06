358a4b38026c2603ae934388844b6bcc180bdcf1
Matt Palmer <devnull@localhost>
2012-06-06 13:57:28 +0100

    Updated javadoc

--- src/net/domesdaybook/searcher/SearchUtils.java
+++ src/net/domesdaybook/searcher/SearchUtils.java
@@ -39,18 +39,31 @@ import net.domesdaybook.matcher.sequence.SequenceMatcher;
 import net.domesdaybook.reader.Reader;
 
 /**
- *
+ * A static utility class holding useful methods in processing searches and
+ * search results.
+ * 
  * @author Matt Palmer
  */
 public final class SearchUtils {
 
 
-    
+    /**
+     * The constructor is private to prevent instantiation of a static utility class.
+     */
     private SearchUtils() {
     }
     
     
-    public static <T> List<SearchResult<T>> searchAllForwards(final Searcher<T> searcher, final byte[] bytes) {
+    /**
+     * Searches a byte array forwards for all matches of a {@link Searcher}.
+     * 
+     * @param <T> The type of object associated with a match in the Searcher.
+     * @param searcher The Searcher to search with.
+     * @param bytes The byte array to search in.
+     * @return A list of SearchResult objects containing all matches found in the byte array.
+     */
+    public static <T> List<SearchResult<T>> searchAllForwards(final Searcher<T> searcher, 
+                                                              final byte[] bytes) {
         final List<SearchResult<T>> results = new ArrayList<SearchResult<T>>();
         final ForwardSearchIterator<T> iterator = new ForwardSearchIterator<T>(searcher, bytes);
         while (iterator.hasNext()) {
@@ -60,7 +73,18 @@ public final class SearchUtils {
     }
 
 
-    public static <T> List<SearchResult<T>> searchAllForwards(final Searcher<T> searcher, final Reader reader) throws IOException {
+    /**
+     * Searches a {@link net.domesdaybook.reader.Reader) forwards for all matches of a {@link Searcher}.
+     * 
+     * @param <T> The type of object associated with a match in the Searcher.
+     * @param searcher The Searcher to search with.
+     * @param reader The Reader to search in.
+     * @return A list of SearchResult objects containing all matches found in the Reader.
+     * @throws IOException if a problem occurred reading in the Reader.
+     */
+    public static <T> List<SearchResult<T>> searchAllForwards(final Searcher<T> searcher, 
+                                                              final Reader reader) 
+            throws IOException {
         final List<SearchResult<T>> results = new ArrayList<SearchResult<T>>();
         final ForwardSearchIterator<T> iterator = new ForwardSearchIterator<T>(searcher, reader);
         while (iterator.hasNext()) {
@@ -70,7 +94,16 @@ public final class SearchUtils {
     }
 
 
-    public static <T> List<SearchResult<T>> searchAllBackwards(final Searcher<T> searcher, final byte[] bytes) {
+    /**
+     * Searches a byte array backwards for all matches of a {@link Searcher}.
+     * 
+     * @param <T> The type of object associated with a match in the Searcher.
+     * @param searcher The Searcher to search with.
+     * @param bytes The byte array to search in.
+     * @return A list of SearchResult objects containing all matches found in the byte array.
+     */
+    public static <T> List<SearchResult<T>> searchAllBackwards(final Searcher<T> searcher,
+                                                               final byte[] bytes) {
         final List<SearchResult<T>> results = new ArrayList<SearchResult<T>>();
         final BackwardSearchIterator<T> iterator = new BackwardSearchIterator<T>(searcher, bytes);
         while (iterator.hasNext()) {
@@ -80,7 +113,18 @@ public final class SearchUtils {
     }
 
 
-    public static <T> List<SearchResult<T>> searchAllBackwards(final Searcher<T> searcher, final Reader reader) throws IOException {
+    /**
+     * Searches a {@link net.domesdaybook.reader.Reader) forwards for all matches of a {@link Searcher}.
+     * 
+     * @param <T> The type of object associated with a match in the Searcher.
+     * @param searcher The Searcher to search with.
+     * @param reader The Reader to search in.
+     * @return A list of SearchResult objects containing all matches found in the Reader.
+     * @throws IOException if a problem occurred reading in the Reader.
+     */
+    public static <T> List<SearchResult<T>> searchAllBackwards(final Searcher<T> searcher, 
+                                                               final Reader reader)
+            throws IOException {
         final List<SearchResult<T>> results = new ArrayList<SearchResult<T>>();
         final BackwardSearchIterator<T> iterator = new BackwardSearchIterator<T>(searcher, reader);
         while (iterator.hasNext()) {
@@ -90,14 +134,31 @@ public final class SearchUtils {
     }
     
     
+    /**
+     * Returns a single SearchResult object from a match position and a matching object.
+     * 
+     * @param <T> The type of object associated with a Searcher match.
+     * @param matchPosition The position the object matched at.
+     * @param matchingObject The object which matched at the position.
+     * @return A list containing a single SearchResult.
+     */
     public static <T> List<SearchResult<T>> singleResult(final long matchPosition,
-        final T matchingObject) {
+                                                         final T matchingObject) {
         final List<SearchResult<T>> result = new ArrayList<SearchResult<T>>(1);
         result.add(new SearchResult<T>(matchPosition, matchingObject));
         return result;
     }
     
     
+    /**
+     * Returns a list of SearchResults for multiple objects all matching at the
+     * same position.
+     * 
+     * @param <T> The type of object associated with a Searcher match.
+     * @param matchPosition The position the objects matched at.
+     * @param matchingObjects The objects which matched at the position.
+     * @return A list containing SearchResults for all objects at the same position.
+     */
     public static <T> List<SearchResult<T>> resultsAtPosition(final long matchPosition,
             final Collection<T> matchingObjects) {
         final List<SearchResult<T>> results = new ArrayList<SearchResult<T>>(matchingObjects.size());
@@ -108,21 +169,19 @@ public final class SearchUtils {
     }
     
     
-    public static List<SearchResult<SequenceMatcher>> resultsFromPosition(
-            final long searchPosition, 
-            final Collection<SequenceMatcher> matchingSequences, 
-            final long furthestLimit) {
-        final List<SearchResult<SequenceMatcher>> results = new ArrayList<SearchResult<SequenceMatcher>>(matchingSequences.size());
-        for (final SequenceMatcher sequence : matchingSequences) {
-            final long finalPosition = searchPosition + sequence.length() - 1;
-            if (finalPosition <= furthestLimit) {
-                results.add(new SearchResult<SequenceMatcher>(searchPosition, sequence));
-            }
-        }
-        return results;
-    }
-    
-    
+    /**
+     * Returns a list of SearchResults for multiple sequences all matching at a
+     * right-aligned position.  The start of each sequence (the actual match position
+     * we will report back) could in theory fall before the start of the search, or
+     * even after the end of the search position.  Any sequences not falling within
+     * the bounds of the search are filered out, and the others returned as matches.
+     * 
+     * @param backFromPosition The right-aligned position at which the sequences match.
+     * @param matchingSequences The sequences which matched.
+     * @param searchStart The start position of the search.
+     * @param searchEnd The end position of the search.
+     * @return A list of search results for all sequences which fit inside the search.
+     */
     public static List<SearchResult<SequenceMatcher>> resultsBackFromPosition(
             final long backFromPosition,
             final Collection<? extends SequenceMatcher> matchingSequences,
@@ -139,23 +198,29 @@ public final class SearchUtils {
     }
     
     
+    /**
+     * Returns a type-safe empty list of SearchResults.
+     * 
+     * @param <T> The type of object associated with a match in the Searcher.
+     * @return An empty list of SearchResult<T>.
+     */
     public static <T> List<SearchResult<T>> noResults() {
         return Collections.emptyList();
     }
     
-    
-    public static <T> List<SearchResult<T>> subtractPositionFromResults (
-            final List<SearchResult<T>> originalResults,
-            final long amountToSubtract) {
-        final List<SearchResult<T>> newResults = new ArrayList<SearchResult<T>>(originalResults.size());
-        for (final SearchResult<T> result : originalResults) {
-            newResults.add(new SearchResult<T>(result.getMatchPosition() - amountToSubtract,
-                                               result.getMatchingObject()));
-        }
-        return newResults;
-    }
-    
-    
+
+    /**
+     * Returns a new list of SearchResults created from another list of SearchResults,
+     * by adding a number to the match position of each SearchResult.
+     * <p>
+     * This is useful to translate a match relative to a Window into a match relative
+     * to the entire Reader.
+     * 
+     * @param <T> The type of object associated with a match in the Searcher.
+     * @param originalResults The original search results to add a number to.
+     * @param amountToAdd The amount to add to the match position of each SearchResult.
+     * @return A list of SearchResults with the match position adjusted by the amountToAdd.
+     */
     public static <T> List<SearchResult<T>> addPositionToResults (
             final List<SearchResult<T>> originalResults,
             final long amountToAdd) {
