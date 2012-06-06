64f618b2ed4a821c3bbc044b4ae02bcdb689f2f2
Matt Palmer <devnull@localhost>
2012-06-06 11:26:42 +0100

    Updated javadoc.

--- src/net/domesdaybook/matcher/multisequence/MultiSequenceReverseMatcher.java
+++ src/net/domesdaybook/matcher/multisequence/MultiSequenceReverseMatcher.java
@@ -39,7 +39,28 @@ import java.util.Map;
 import net.domesdaybook.matcher.sequence.SequenceMatcher;
 import net.domesdaybook.reader.Reader;
 
+//TODO: should this simply be a translation class with no knowledge of the
+//      reversal process?  Mapping the sequences in one multisequencematcher
+//      to those in another?  Might need a transformation object (verb...!)
+//      or leave transformation to an external process, and just pass in the 
+//      map of old to new sequences.
+
 /**
+ * This class matches the reverse of a set of sequences matched by another MultiSequenceMatcher,
+ * but on a match, returns the original non-reversed sequences instead of the reversed ones.
+ * <p>
+ * It wraps a new MultiSequenceMatcher of the type originally passed in, but
+ * with the sequences matched by the original reversed.  Since it is possible to 
+ * reverse any MultiSequenceMatcher by calling its {@link #reverse()} method, 
+ * there would seem little use for a class like this.  However, it has one further
+ * trick: the sequences returned by a match on this class are translated back into
+ * the non-reversed sequences used by the original MultiSequenceMatcher passed in.
+ * <p>
+ * This may seem like a strange ability to need.  However, it is very useful when
+ * using shift-based multi-sequence search techniques 
+ * (see @link net.domesdaybook.searcher.multisequence}) 
+ * to be able to match the reverse of a set of sequences, but to know which of the 
+ * original un-reversed sequences were "matched" in reverse.
  *
  * @author Matt Palmer
  */
@@ -48,6 +69,11 @@ public final class MultiSequenceReverseMatcher implements MultiSequenceMatcher {
     private final MultiSequenceMatcher reversed;
     private final Map<SequenceMatcher, SequenceMatcher> reverseToOriginals;
     
+    /**
+     * Constructs a MultiSequenceReverseMatcher from an original MultiSequenceMatcher.
+     * 
+     * @param original The original MultiSequenceMatcher.
+     */
     public MultiSequenceReverseMatcher(final MultiSequenceMatcher original) {
         reverseToOriginals = new IdentityHashMap<SequenceMatcher, SequenceMatcher>();
         final List<SequenceMatcher> originalSequences = original.getSequenceMatchers();
@@ -61,80 +87,167 @@ public final class MultiSequenceReverseMatcher implements MultiSequenceMatcher {
     }
     
     
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public Collection<SequenceMatcher> allMatches(Reader reader, long matchPosition) throws IOException {
         return getOriginalSequences(reversed.allMatches(reader, matchPosition));
     }
 
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public Collection<SequenceMatcher> allMatches(byte[] bytes, int matchPosition) {
         return getOriginalSequences(reversed.allMatches(bytes, matchPosition));
     }
 
+    
+    /**
+     * {@inheritDoc}
+     */    
+    @Override
     public Collection<SequenceMatcher> allMatchesBackwards(Reader reader, long matchPosition) throws IOException {
         return getOriginalSequences(reversed.allMatchesBackwards(reader, matchPosition));
     }
 
+    
+    /**
+     * {@inheritDoc}
+     */    
+    @Override
     public Collection<SequenceMatcher> allMatchesBackwards(byte[] bytes, int matchPosition) {
         return getOriginalSequences(reversed.allMatchesBackwards(bytes, matchPosition));
     }
 
+    
+    /**
+     * {@inheritDoc}
+     */    
+    @Override
     public SequenceMatcher firstMatch(Reader reader, long matchPosition) throws IOException {
         return getOriginalSequence(reversed.firstMatch(reader, matchPosition));
     }
 
+    
+    /**
+     * {@inheritDoc}
+     */    
+    @Override
     public SequenceMatcher firstMatch(byte[] bytes, int matchPosition) {
         return getOriginalSequence(reversed.firstMatch(bytes, matchPosition));
     }
 
+    
+    /**
+     * {@inheritDoc}
+     */    
+    @Override
     public SequenceMatcher firstMatchBackwards(Reader reader, long matchPosition) throws IOException {
         return getOriginalSequence(reversed.firstMatchBackwards(reader, matchPosition));
     }
 
+    
+    /**
+     * {@inheritDoc}
+     */    
+    @Override
     public SequenceMatcher firstMatchBackwards(byte[] bytes, int matchPosition) {
         return getOriginalSequence(reversed.firstMatchBackwards(bytes, matchPosition));
     }
 
+    
+    /**
+     * {@inheritDoc}
+     */    
+    @Override
     public boolean matchesBackwards(Reader reader, long matchPosition) throws IOException {
         return reversed.matchesBackwards(reader, matchPosition);
     }
-
+    
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public boolean matchesBackwards(byte[] bytes, int matchPosition) {
         return reversed.matchesBackwards(bytes, matchPosition);
     }
 
+    
+    /**
+     * {@inheritDoc}
+     */    
+    @Override
     public int getMinimumLength() {
         return reversed.getMinimumLength();
     }
 
+    
+    /**
+     * {@inheritDoc}
+     */    
+    @Override
     public int getMaximumLength() {
         return reversed.getMaximumLength();
     }
-
+    
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public MultiSequenceMatcher reverse() {
         return new MultiSequenceReverseMatcher(reversed);
     }
     
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public MultiSequenceMatcher newInstance(Collection<? extends SequenceMatcher> sequences) {
         return new MultiSequenceReverseMatcher(reversed.newInstance(sequences));
     }
     
 
+    /**
+     * {@inheritDoc}
+     */    
+    @Override
     public List<SequenceMatcher> getSequenceMatchers() {
         return reversed.getSequenceMatchers();
     }
     
 
+    /**
+     * {@inheritDoc}
+     */    
+    @Override
     public boolean matches(Reader reader, long matchPosition) throws IOException {
         return reversed.matches(reader, matchPosition);
     }
     
-
+    
+    /**
+     * {@inheritDoc}
+     */    
+    @Override
     public boolean matches(byte[] bytes, int matchPosition) {
         return reversed.matches(bytes, matchPosition);
     }
     
     
-    private Collection<SequenceMatcher> getOriginalSequences(Collection<SequenceMatcher> reversed) {
+    /**
+     * Translates a collection of reversed sequence matchers back into the original
+     * non-reversed ones they were created from.
+     * 
+     * @param reversed A collection of reversed sequence matchers.
+     * @return A collection of the corresponding non-reversed sequence matchers.
+     */
+    private Collection<SequenceMatcher> getOriginalSequences(final Collection<SequenceMatcher> reversed) {
         final List<SequenceMatcher> originals = new ArrayList<SequenceMatcher>(reversed.size());
         for (final SequenceMatcher reverse : reversed) {
             originals.add(reverseToOriginals.get(reverse));
@@ -143,7 +256,7 @@ public final class MultiSequenceReverseMatcher implements MultiSequenceMatcher {
     }
     
     
-    private SequenceMatcher getOriginalSequence(SequenceMatcher reversed) {
+    private SequenceMatcher getOriginalSequence(final SequenceMatcher reversed) {
         return reverseToOriginals.get(reversed);
     }
 
