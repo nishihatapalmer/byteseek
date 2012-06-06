38424792f13a994ce815252482f7dd11fe264f66
Matt Palmer <devnull@localhost>
2012-06-06 10:17:47 +0100

    Updated javadoc.

--- src/net/domesdaybook/reader/StringReader.java
+++ src/net/domesdaybook/reader/StringReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Matt Palmer 2011, All rights reserved.
+ * Copyright Matt Palmer 2011-2012, All rights reserved.
  *
  * This code is licensed under a standard 3-clause BSD license:
  *
@@ -33,10 +33,15 @@ package net.domesdaybook.reader;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.UnsupportedCharsetException;
 import net.domesdaybook.reader.cache.NoCache;
 
 /**
- *
+ * A {@link Reader} which gives access to the bytes of a String, either
+ * using the default platform encoding, or using a specific {@link java.nio.charset.Charset}.
+ * <p>
+ * This Reader is thread-safe, as it is immutable.
+ * 
  * @author Matt Palmer
  */
 public class StringReader extends AbstractReader {
@@ -45,21 +50,24 @@ public class StringReader extends AbstractReader {
     private final Charset charset;
     
     
-    
     /**
+     * Constructs a StringReader from a {@link java.lang.String}, using the
+     * platform default {@link java.nio.charset.Charset} to encode the bytes of the String.
      * 
-     * @param string
+     * @param string The String to read using the platform specific charset encoding.
      */
     public StringReader(final String string) {
         this(string, Charset.defaultCharset());
     }
 
     
-    
     /**
+     * Constructs a StringReader from a {@link java.lang.String}, using the
+     * supplied {@link java.nio.charset.Charset} to encode the bytes of the String.
      * 
-     * @param string
-     * @param charsetName
+     * @param string The String to read
+     * @param charsetName The name of the Charset to use when encoding the bytes of the String.
+     * @throws UnsupportedCharsetException If the charset name is not supported.
      */
     public StringReader(final String string, final String charsetName) {
         this(string, Charset.forName(charsetName));
@@ -88,12 +96,22 @@ public class StringReader extends AbstractReader {
     }
     
     
+    /**
+     * Creates a new Window every time it is called, consisting of a Window
+     * onto the entire byte array that encodes the String.
+     */
     @Override
     Window createWindow(final long windowStart) throws IOException {
         return new Window(bytes, 0, bytes.length);
     }
 
     
+    /**
+     * Returns the length of the bytes that encode the String.  This may not be
+     * the same length as the number of characters in the original String.
+     * <p>
+     * It will never throw an IOException, although other Readers may.
+     */
     @Override
     public long length() throws IOException {
         return bytes.length;
@@ -101,8 +119,9 @@ public class StringReader extends AbstractReader {
     
     
     /**
+     * Returns a new String based on the byte encoding and Charset used.
      * 
-     * @return
+     * @return A new String that replicates the original String used to construct this Reader.
      */
     public String getString() {
         return new String(bytes, charset);
@@ -110,8 +129,9 @@ public class StringReader extends AbstractReader {
     
     
     /**
+     * Returns the {@link java.nio.charset.Charset} used to encode the bytes in this Reader.
      * 
-     * @return
+     * @return The Charset used to encode the bytes in this Reader.
      */
     public Charset getCharset() {
         return charset;
