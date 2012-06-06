bc95e0bd2e7e0c651efe0e2c0e7b173a59eaf958
Matt Palmer <devnull@localhost>
2012-06-06 10:08:18 +0100

    Updated javadoc.

--- src/net/domesdaybook/reader/ReadUtils.java
+++ src/net/domesdaybook/reader/ReadUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Matt Palmer 2011, All rights reserved.
+ * Copyright Matt Palmer 2011-2012, All rights reserved.
  *
  * This code is licensed under a standard 3-clause BSD license:
  *
@@ -39,24 +39,33 @@ import java.io.OutputStream;
 import java.io.RandomAccessFile;
 
 /**
- *
- * @author matt
+ * A static utility package containing some useful methods for reading and writing bytes
+ * using InputStreams, OutputStreams, and RandomAccessFiles.
+ * 
+ * @author M\tt Palmer
  */
 public final class ReadUtils {
     
     private static int DEFAULT_BUFFER_SIZE = 4096;
     
+    /**
+     * Private constructor to prevent instantiating a utility class.
+     */
     private ReadUtils() {
     }
     
     
     
     /**
+     * Reads bytes from an {@link java.io.InputStream} into the byte array,
+     * until the byte array is filled or there are no more bytes in the stream.
+     * <p>
+     * Returns the total number of bytes read into the array.
      * 
-     * @param input
-     * @param bytes
-     * @return
-     * @throws IOException
+     * @param input The InputStream to read from.
+     * @param bytes The byte array to fill.
+     * @return int The total number of bytes read.
+     * @throws IOException If a problem occurs reading from the InputStream.
      */
     public static int readBytes(final InputStream input, final byte[] bytes) throws IOException {
         final int blockSize = bytes.length;
@@ -71,11 +80,16 @@ public final class ReadUtils {
     
     
     /**
+     * Reads bytes from a {@link java.io.RandomAccessFile} into the byte array,
+     * starting from the current seek position of the RandomAccessFile,
+     * until the byte array is filled or there are no more bytes in the RandomAccessFile.
+     * <p>
+     * Returns the total number of bytes read into the array.
      * 
-     * @param input
-     * @param bytes
-     * @return
-     * @throws IOException
+     * @param input The RandomAccessFile to read from.
+     * @param bytes The byte array to fill.
+     * @return int The total number of bytes read.
+     * @throws IOException If a problem occurs reading from the RandomAccessFile.
      */
     public static int readBytes(final RandomAccessFile input, final byte[] bytes) throws IOException {
         final int blockSize = bytes.length;
@@ -90,12 +104,17 @@ public final class ReadUtils {
     
     
     /**
+     * Reads bytes from a {@link java.io.RandomAccessFile} into the byte array,
+     * starting from the position provided in the RandomAccessFile,
+     * until the byte array is filled or there are no more bytes in the RandomAccessFile.
+     * <p>
+     * Returns the total number of bytes read into the array.
      * 
-     * @param input
-     * @param bytes
-     * @param fromPosition
-     * @return
-     * @throws IOException
+     * @param input The RandomAccessFile to read from.
+     * @param bytes The byte array to fill.
+     * @param fromPosition The position to begin reading from in the RandomAccessFile.
+     * @return int The total number of bytes read.
+     * @throws IOException If a problem occurs reading from the RandomAccessFile.
      */
     public static int readBytes(final RandomAccessFile input, final byte[] bytes, 
             final long fromPosition) throws IOException {
@@ -110,12 +129,14 @@ public final class ReadUtils {
         return totalRead;
     }
     
+    
     /**
+     * Writes the contents of an array of bytes into a {@link java.io.RandomAccessFile}.
      * 
-     * @param output
-     * @param bytes
-     * @param atPosition
-     * @throws IOException
+     * @param output The RandomAccessFile to write the bytes into.
+     * @param bytes The array of bytes to be written.
+     * @param atPosition The position to write the bytes into.
+     * @throws IOException If a problem occurs writing the bytes into the RandomAccessFile.
      */
     public static void writeBytes(final RandomAccessFile output, final byte[] bytes, 
             final long atPosition) throws IOException {
@@ -125,9 +146,11 @@ public final class ReadUtils {
     
     
     /**
+     * Creates a temporary file in the default temporary file area, with the
+     * filename prefix "byteseek" and a filename extension of ".tmp".
      * 
-     * @return
-     * @throws IOException
+     * @return The temporary file created.
+     * @throws IOException If an error occurs creating the temporary file.
      */
     public static File createTempFile() throws IOException {
         return File.createTempFile("byteseek", ".tmp");
@@ -135,10 +158,14 @@ public final class ReadUtils {
     
     
     /**
+     * Creates a temporary file in the default temporary file area, with the
+     * filename prefix "byteseek" and a filename extension of ".tmp".  The 
+     * temporary file will have the contents of the {@link java.io.InputStream} written to it,
+     * using a default buffer size of 4096.
      * 
-     * @param in
-     * @return
-     * @throws IOException
+     * @param in The InputStream to copy.
+     * @return A temporary file containing the contents of the InputStream.
+     * @throws IOException If an error occurs creating the temporary file.
      */
     public static File createTempFile(final InputStream in) throws IOException {
         return createTempFile(in, DEFAULT_BUFFER_SIZE);
@@ -146,11 +173,15 @@ public final class ReadUtils {
     
     
     /**
+     * Creates a temporary file in the default temporary file area, with the
+     * filename prefix "byteseek" and a filename extension of ".tmp".  The 
+     * temporary file will have the contents of the {@link java.io.InputStream} written to it,
+     * using the buffer size provided.
      * 
-     * @param in
-     * @param bufferSize
-     * @return
-     * @throws IOException
+     * @param in The InputStream to copy.
+     * @param bufferSize The size of the buffer to use when copying the stream.
+     * @return A temporary file containing the contents of the InputStream.
+     * @throws IOException If an error occurs creating the temporary file.
      */
     public static File createTempFile(final InputStream in, final int bufferSize) throws IOException {
         final File tempFile = createTempFile();
@@ -162,10 +193,12 @@ public final class ReadUtils {
         
     
     /**
+     * Copies the contents of an {@link java.io.InputStream} to an {@link java.io.OutputStream}, 
+     * using the default buffer size of 4096.
      * 
-     * @param in
-     * @param out
-     * @throws IOException
+     * @param in The InputStream to copy.
+     * @param out The OutputStream to copy to.
+     * @throws IOException If a problem occurs copying the InputStream to the OutputStream.
      */
     public static void copyStream(final InputStream in,
                                   final OutputStream out) throws IOException {
@@ -174,11 +207,13 @@ public final class ReadUtils {
     
     
     /**
+     * Copies the contents of an {@link java.io.InputStream} to an {@link java.io.OutputStream}, 
+     * using buffer size provided.
      * 
-     * @param in
-     * @param out
-     * @param bufferSize
-     * @throws IOException
+     * @param in The InputStream to copy.
+     * @param out The OutputStream to copy to.
+     * @param bufferSize The size of the buffer to use when copying the stream.
+     * @throws IOException If a problem occurs copying the InputStream to the OutputStream.
      */
     public static void copyStream(final InputStream in, 
                                   final OutputStream out,
