90750bf7e6bb885da5f31d2d2999089e28e7c880
Matt Palmer <devnull@localhost>
2012-08-19 16:16:12 +0100

    Added nodes to complete value types.

---
+++ src/net/domesdaybook/parser/tree/node/IntNode.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright Matt Palmer 2012, All rights reserved.
+ *
+ * This code is licensed under a standard 3-clause BSD license:
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution.
+ * 
+ *  * The names of its contributors may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.domesdaybook.parser.tree.node;
+
+import net.domesdaybook.parser.ParseException;
+import net.domesdaybook.parser.tree.ParseTreeType;
+
+
+public class IntNode extends BaseNode {
+
+  private int value;
+
+  public IntNode() {
+    this(0, false);
+  }
+  
+  public IntNode(final int value) {
+    this(value, false);
+  }
+
+  public IntNode(final int value,
+                  final boolean inverted) {
+    super(ParseTreeType.INTEGER, inverted);
+    this.value = value;
+  }
+
+  @Override
+  public int getIntValue() throws ParseException {
+    return value;
+  }
+  
+  public void setIntValue(final int value) {
+    this.value = value;
+  }
+
+}
