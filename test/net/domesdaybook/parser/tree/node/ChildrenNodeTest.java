9c02664c356e10b51e27d52e6a93ef966b90b125
Matt Palmer <devnull@localhost>
2012-10-29 19:29:38 +0000

    Added tests for addChild and removeChild with differently constructed child lists.

--- test/net/domesdaybook/parser/tree/node/ChildrenNodeTest.java
+++ test/net/domesdaybook/parser/tree/node/ChildrenNodeTest.java
@@ -13,6 +13,7 @@ import net.domesdaybook.parser.tree.ParseTree;
 
 import org.junit.Test;
 import net.domesdaybook.parser.tree.ParseTreeType;
+import net.domesdaybook.parser.tree.node.ChildrenNode.ListStrategy;
 
 /**
  * @author Matt Palmer
@@ -20,63 +21,108 @@ import net.domesdaybook.parser.tree.ParseTreeType;
  */
 public class ChildrenNodeTest {
 
-	//TODO: add tests for setChildren, addChild, removeChild
-	//      and constructors which create their own lists of children.
-	
 	@Test
-	public final void testChildrenNode() {
+	public final void testDifferentNumbersOfChildren() {
+		for (int numChildren = 0; numChildren < 10; numChildren++) {
+			runNumChildrenTests(numChildren);
+		}
+	}
+	
+	private void runNumChildrenTests(int numChildren) {
 		List<ParseTree> children = new ArrayList<ParseTree>();
-		runTests(children, 0);
-		
-		children.add(new BaseNode(ParseTreeType.ANY));
-		runTests(children, 1);
-		
-		children.add(new BaseNode(ParseTreeType.BYTE));
-		runTests(children, 2);
+		for (int i  = 0; i < numChildren; i++) {
+			children.add(new BaseNode(ParseTreeType.ANY));
+		}
+		runTests(children, numChildren);
 	}
 	
 	private void runTests(List<ParseTree> children, int numChildren) {
 		for (ParseTreeType type : ParseTreeType.values()) {
-			testInvertedStatus(type, children, numChildren);
+			testConstructors(type, children, numChildren);
+			testChangingChildren(type, children, numChildren);
 		}		
 	}
 	
-	private void testInvertedStatus(ParseTreeType type, List<ParseTree> children, int numChildren) {
+	private void testConstructors(ParseTreeType type, List<ParseTree> children, int numChildren) {
 		ChildrenNode node = new ChildrenNode(type, children);
-		testNode(node, type, numChildren, false);
+		testNode("Default false inversion: ", node, type, numChildren, false);
 
 		node = new ChildrenNode(type, children, false);
-		testNode(node, type, numChildren, false);
+		testNode("Specified false inversion: ", node, type, numChildren, false);
 		
 		node = new ChildrenNode(type, children, true);
-		testNode(node, type, numChildren, true);		
+		testNode("Specified true inversion: ", node, type, numChildren, true);		
 	}
 	
-	private void testNode(ChildrenNode node, ParseTreeType type, int numChildren, boolean isInverted) {
-		assertEquals("ChildrenNode has correct type: " + type, node.getParseTreeType(), type);
+	private void testChangingChildren(ParseTreeType type, List<ParseTree> children, int numChildren) {
+		List<ParseTree> childrenToTest = new ArrayList<ParseTree>(children);
+		
+		ChildrenNode defaultNode = new ChildrenNode(type, childrenToTest); 
+		ChildrenNode copyNode    = new ChildrenNode(type, childrenToTest, ListStrategy.COPY_LIST);
+		ChildrenNode givenNode   = new ChildrenNode(type, childrenToTest, ListStrategy.USE_GIVEN_LIST);
+
+		assertEquals("Before change: Default behaviour is to copy the child list.", numChildren, defaultNode.getChildren().size());
+		assertEquals("Before change: Specified copy behaviour also copies the list.", numChildren, copyNode.getChildren().size());
+		assertEquals("Before change: Specified use given behaviour has same children now.", numChildren, givenNode.getChildren().size());
+		
+		ParseTree nodeToAdd = new StringNode("Node to add");
+		childrenToTest.add(nodeToAdd);
+		
+		assertEquals("Add to test list: Default behaviour is to copy the child list.", numChildren, defaultNode.getChildren().size());
+		assertEquals("Add to test list: Specified copy behaviour also copies the list.", numChildren, copyNode.getChildren().size());
+		assertEquals("Add to test list: Specified use given behaviour has more children now.", numChildren + 1, givenNode.getChildren().size());
+		
+		ParseTree nodeToAdd2 = new StringNode("Second node to add");
+		defaultNode.addChild(nodeToAdd2);
+		copyNode.addChild(nodeToAdd2);
+		givenNode.addChild(nodeToAdd2);
+		
+		assertEquals("Add to default node: Default behaviour is to copy the child list.", numChildren + 1, defaultNode.getChildren().size());
+		assertEquals("Add to copy node: Specified copy behaviour also copies the list.", numChildren + 1, copyNode.getChildren().size());
+		assertEquals("Add to given node: Specified use given behaviour has more children now.", numChildren + 2, givenNode.getChildren().size());
+		assertEquals("Add to given node: test list is also increased", numChildren + 2, childrenToTest.size());
+		
+		childrenToTest.remove(nodeToAdd);
+		
+		assertEquals("Remove node from test list: Default behaviour is to copy the child list.", numChildren + 1, defaultNode.getChildren().size());
+		assertEquals("Remove node from test list: Specified copy behaviour also copies the list.", numChildren + 1, copyNode.getChildren().size());
+		assertEquals("Remove node from test list: Specified use given behaviour has less children now.", numChildren + 1, givenNode.getChildren().size());
+
+		defaultNode.removeChild(nodeToAdd2);
+		copyNode.removeChild(nodeToAdd2);
+		givenNode.removeChild(nodeToAdd2);
+		
+		assertEquals("Remove from default node: Default behaviour is is back to start", numChildren, defaultNode.getChildren().size());
+		assertEquals("Remove from copy node: Specified copy behaviour is back to start.", numChildren, copyNode.getChildren().size());
+		assertEquals("Remove from given node: Specified use given behaviour is back to start.", numChildren, givenNode.getChildren().size());
+		assertEquals("Remove from given node: test list is back to start", numChildren, childrenToTest.size());
+	}
+	
+	private void testNode(String description, ChildrenNode node, ParseTreeType type, int numChildren, boolean isInverted) {
+		assertEquals(description + "ChildrenNode has correct type: " + type, node.getParseTreeType(), type);
 		try {
-			assertEquals("ChildrenNode value is correct inversion: " + isInverted, isInverted, node.isValueInverted());
+			assertEquals(description + "ChildrenNode value is correct inversion: " + isInverted, isInverted, node.isValueInverted());
 		} catch (ParseException e) {
-			fail("ChildrenNode should not throw a ParseException if asked if the value is inverted.");
+			fail(description + "ChildrenNode should not throw a ParseException if asked if the value is inverted.");
 		}
 		
 		try { 
 			node.getByteValue();
-			fail("Expected a ParseException if asked for the byte value");
+			fail(description + "Expected a ParseException if asked for the byte value");
 		} catch (ParseException allIsFine) {};
 		
 		try { 
 			node.getIntValue();
-			fail("Expected a ParseException if asked for the int value");
+			fail(description + "Expected a ParseException if asked for the int value");
 		} catch (ParseException allIsFine) {};
 		
 		try { 
 			node.getTextValue();
-			fail("Expected a ParseException if asked for the text value");
+			fail(description + "Expected a ParseException if asked for the text value");
 		} catch (ParseException allIsFine) {};
 		
-		assertNotNull("Child list is not null", node.getChildren());
-		assertEquals("Child list has correct number of children " + numChildren, numChildren, node.getChildren().size());
+		assertNotNull(description + "Child list is not null", node.getChildren());
+		assertEquals(description + "Child list has correct number of children " + numChildren, numChildren, node.getChildren().size());
 	}
 
 }
