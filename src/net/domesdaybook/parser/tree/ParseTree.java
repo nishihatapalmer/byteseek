08204d5f9c49c70439c6e0974e5a89ee9694ca0c
Matt Palmer <devnull@localhost>
2012-10-30 11:07:08 +0000

    Update javadoc.

--- src/net/domesdaybook/parser/tree/ParseTree.java
+++ src/net/domesdaybook/parser/tree/ParseTree.java
@@ -40,15 +40,16 @@ import net.domesdaybook.parser.Parser;
  * An interface for an Abstract Syntax Tree (AST) produced by a {@link Parser},
  * <p>
  * Each instance of this interface represents a node in the AST.  Nodes can 
- * be roughly divided into value-carrying nodes (which represent a byte value
- * or values to match), and structural nodes (which represent instructions to
+ * be roughly divided into value-carrying nodes (which represent a byte, integer
+ * or text value to match), and structural nodes (which represent instructions to
  * perform operations on the child nodes or to otherwise interpret their value).
  * <p>
  * In general, value carrying nodes are leaf-nodes (which have no child nodes), and
- * structural nodes have children, but no direct value.  A value carrying node 
+ * structural nodes have children, but no direct value. Nodes
  * may optionally be 'inverted', in which case the value it possesses should be
  * inverted to match.  For example, a single byte value node with the value 'FF',
- * which is also inverted should match everything except 'FF'.
+ * which is also inverted should match everything except 'FF'.  Note that inversion
+ * doesn't make sense for integer or text nodes.
  * 
  * @author Matt Palmer
  *
@@ -56,9 +57,9 @@ import net.domesdaybook.parser.Parser;
 public interface ParseTree {
 
   /**
-   * Returns the type of the ParseTree node.  Each type of node 
-   * must specify the expected implementation of the other methods in 
-   * this interface, in its JavaDoc.
+   * Returns the type of the ParseTree node. The {@link ParseTreeType} of a
+   * node defines the expected implementation of the methods in 
+   * this interface, which is specified in the JavaDoc for each ParseTreeType.
    * 
    * @return ParseTreeType The type of the ParseTreeNode.
    */
@@ -71,25 +72,55 @@ public interface ParseTree {
 	 * 
 	 * @return A byte value of this node.
 	 * @throws ParseException If no such byte value exists or another 
-	 *         problem occurs parsing the value.
+	 *          problem occurs parsing the value.
 	 */
 	public byte getByteValue() throws ParseException;
 
 	/**
 	 * Returns an integer representing the value of this node,
-	 * or throws a ParseException if no such byte value exists,
+	 * or throws a ParseException if no such int value exists,
 	 * or another problem occurs parsing the value.
 	 * 
 	 * @return An integer value of this node.
-	 * @throws ParseException If not such integer value exists or another
-	 *         problem occurs parsing the value.
+	 * @throws ParseException If no such integer value exists or another
+	 *          problem occurs parsing the value.
 	 */
 	public int getIntValue() throws ParseException;
 
+	/**
+	 * Returns a String representing the value of this node,
+	 * or throws a ParseException if no such String value exists,
+	 * or another problem occurs parsing the value.
+	 * 
+	 * @return A String value of this node.
+	 * @throws ParseException If no such String value exists or another
+	 *          problem occurs parsing the value.
+	 */
 	public String getTextValue() throws ParseException;
 	
+	/**
+	 * Returns whether the value of this node should be inverted or not.
+	 * 
+	 * @return boolean True if the value of this node should be inverted by a compiler.
+	 * @throws ParseException If a problem occurs determining the inversion status.
+	 */
 	public boolean isValueInverted() throws ParseException;
 
+	/**
+	 * Returns a list of child nodes for this node.  If there are no child nodes,
+	 * then this method will return an empty list.
+	 * <p>
+	 * No assumptions should be made concerning the mutability of the list of
+	 * child nodes returned (an immutable list could be returned).  Equally,
+	 * no assumptions should be made as to whether the list has been defensively
+	 * copied by the ParseTree or not.  It may be mutable, but an internal list.
+	 * <p>
+	 * Extreme care must be taken if deciding the modify a list returned by this method,
+	 * and in general should not be done unless you are very sure that there will
+	 * be no adverse side-effects.
+	 * 
+	 * @return A list of child nodes, or an empty list if there are no child nodes.
+	 */
 	public List<ParseTree> getChildren();
 
 }
