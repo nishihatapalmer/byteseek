9f3a27cbb0e59c5da34eebcc2453b55b18408d8e
Matt Palmer <devnull@localhost>
2013-02-11 21:39:16 +0000

    added test stub for string parse reader

---
+++ test/net/domesdaybook/parser/StringParseReaderTest.java
@@ -0,0 +1,79 @@
+package net.domesdaybook.parser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringParseReaderTest {
+
+	@Test
+	public final void testStringParseReader() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetPosition() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAtEnd() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testRead() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testLastRead() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testPeekAhead() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testReadHexByte() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testReadHexByteInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testReadInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testReadString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testReadToChar() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testFindNextChar() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testReadBoundsChecked() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
