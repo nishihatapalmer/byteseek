cd52c1b4cefa6988dfaa52e6abb0c35fbde96840
Matt Palmer <devnull@localhost>
2013-09-29 15:35:40 +0100

    Added CollUtils tests.

--- test/net/byteseek/util/collections/CollUtilsTest.java
+++ test/net/byteseek/util/collections/CollUtilsTest.java
@@ -1,6 +1,12 @@
 package net.byteseek.util.collections;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 
 import org.junit.Test;
 
@@ -8,7 +14,35 @@ public class CollUtilsTest {
 
 	@Test
 	public final void testContainsAny() {
-		fail("Not yet implemented"); // TODO
+		try {
+			CollUtils.containsAny(null, null);
+			fail("Expected an Illegal Argument Exception for null collections");
+		} catch (IllegalArgumentException expected) {}
+
+		try {
+			CollUtils.containsAny(new ArrayList<Integer>(), null);
+			fail("Expected an Illegal Argument Exception for null collections");
+		} catch (IllegalArgumentException expected) {}
+
+		
+		try {
+			CollUtils.containsAny(null, new HashSet<Byte>());
+			fail("Expected an Illegal Argument Exception for null collections");
+		} catch (IllegalArgumentException expected) {}
+		
+		Collection<Integer> collection = new ArrayList<Integer>();
+		Collection<Integer> values = new HashSet<Integer>();
+		assertFalse("empty collections do not contain any of each other", CollUtils.containsAny(collection,values));
+		assertFalse("empty collections do not contain any of each other", CollUtils.containsAny(values, collection));
+		
+		collection.add(1);
+		assertFalse("empty values do not contain anything", CollUtils.containsAny(collection,values));
+		collection.add(2);
+		assertFalse("empty values do not contain anything", CollUtils.containsAny(collection,values));
+		
+		values.add(2);
+		assertTrue("both collections share 2", CollUtils.containsAny(collection,values));
+		assertTrue("both collections share 2", CollUtils.containsAny(values, collection));
 	}
 
 }
