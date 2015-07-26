9972ddebfee8a9cfd3a23f7783c864d7896b502b
Matt Palmer <mattpalms@gmail.com>
2015-07-26 14:46:32 +0100

    Put Spec classes in their own units.

---
+++ droidSig/src/main/java/net/byteseek/utils/droid/FragmentSpec.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright Matt Palmer 2015, All rights reserved.
+ *
+ * This code is licensed under a standard 3-clause BSD license:
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * The names of its contributors may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+package net.byteseek.utils.droid;
+
+public class FragmentSpec {
+    public int position;
+    public String fragmentExpression;
+    public int minFragOffset;
+    public int maxFragOffset;
+
+    public String toDROIDXML(String elementName) {
+        StringBuilder builder = new StringBuilder(2048);
+        toDROIDXML(builder, elementName);
+        return builder.toString();
+    }
+
+    public void toDROIDXML(StringBuilder builder, String elementName) {
+        builder.append("\t\t<").append(elementName).append(' ');
+        builder.append("MaxOffset=\"").append(maxFragOffset).append("\" ");
+        builder.append("MinOffset=\"").append(minFragOffset).append("\" ");
+        builder.append("Position=\"").append(position).append("\">");
+        builder.append(StringUtils.escapeXml(fragmentExpression));
+        builder.append("</").append(elementName).append(">\n");
+    }
+}
