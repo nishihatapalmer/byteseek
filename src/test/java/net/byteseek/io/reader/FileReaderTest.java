22da3936e577049bef99e6bf2ceaa50e114bf649
Matt Palmer <mattpalms@gmail.com>
2015-11-12 21:54:46 +0000

    Add copyright and license.

--- src/test/java/net/byteseek/io/reader/FileReaderTest.java
+++ src/test/java/net/byteseek/io/reader/FileReaderTest.java
@@ -1,6 +1,32 @@
 /*
- * Copyright Matt Palmer 2011, All rights reserved.
+ * Copyright Matt Palmer 2011-2015, All rights reserved.
  *
+ * This code is licensed under a standard 3-clause BSD license:
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * The names of its contributors may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 package net.byteseek.io.reader;
@@ -179,7 +205,7 @@ public class FileReaderTest {
 	@Test
 	public void testCreateWindow() throws Exception {
 		
-		//fail("The test case is a prototype.");
+		fail("The test case is a prototype.");
 	}
 
 	/**
@@ -187,7 +213,7 @@ public class FileReaderTest {
 	 */
 	@Test
 	public void testClose() throws Exception {
-		//fail("The test case is a prototype.");
+		fail("The test case is a prototype.");
 	}
 
 	/**
@@ -195,7 +221,7 @@ public class FileReaderTest {
 	 */
 	@Test
 	public void testGetFile() {
-		//fail("The test case is a prototype.");
+		fail("The test case is a prototype.");
 	}
 
 	private void test(FileReader reader, long position, byte value) throws IOException {
@@ -209,6 +235,18 @@ public class FileReaderTest {
 
 	private String getFilePath(final String resourceName) {
 		return this.getClass().getResource(resourceName).getPath();
+	}
+
+	@Test
+	public void testUseSoftWindows() throws Exception {
+		fail("not implemented yet");
+
+	}
+
+	@Test
+	public void testReloadWindowBytes() throws Exception {
+		fail("not implemented yet");
+
 	}
 
 	/**
