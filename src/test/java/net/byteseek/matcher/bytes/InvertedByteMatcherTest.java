b044a098f093e8892941d5e99e72d8e3d2d4330c
Matt Palmer <mattpalms@gmail.com>
2016-02-01 20:24:08 +0000

    Update license and copyright.

--- src/test/java/net/byteseek/matcher/bytes/InvertedByteMatcherTest.java
+++ src/test/java/net/byteseek/matcher/bytes/InvertedByteMatcherTest.java
@@ -1,8 +1,33 @@
 /*
- * Copyright Matt Palmer 2011, All rights reserved.
+ * Copyright Matt Palmer 2011-2016, All rights reserved.
  *
+ * This code is licensed under a standard 3-clause BSD license:
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * The names of its contributors may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
-
 package net.byteseek.matcher.bytes;
 
 import net.byteseek.matcher.bytes.InvertedByteMatcher;
@@ -44,7 +69,7 @@ public class InvertedByteMatcherTest {
    
             assertArrayEquals("matching bytes", matchingBytes, matcher.getMatchingBytes());
             
-            final String regularExpression = String.format("[^%02x]", theByte);
+            final String regularExpression = String.format("^%02x", theByte);
             assertEquals("regular expression", regularExpression, matcher.toRegularExpression(false));
             for (int x = 0; x < 256; x++) {
                 if (x != i) {
@@ -59,5 +84,6 @@ public class InvertedByteMatcherTest {
         }
 
     }
+
 
 }
