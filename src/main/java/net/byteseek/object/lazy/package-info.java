a6fb81cbf10c618bc2f21c5961228ad5eb52d5df
Matt Palmer <mattpalms@gmail.com>
2016-02-24 22:19:37 +0000

    Update Javadoc.

---
+++ src/main/java/net/byteseek/object/lazy/package-info.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright Matt Palmer 2016, All rights reserved.
+ *
+ * This code is licensed under a standard 3-clause BSD license:
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution.
+ * 
+ *  * The names of its contributors may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * A package containing a simple interface for getting objects lazily.
+ * Various implementations of the lazy object are given which allow different
+ * threading strategies to be applied to object creation.
+ * The SingleCheckLazyObject is not entirely thread-safe, and uses
+ * no synchronization, but may be usable in some scenarios.
+ * The DoubleCheckLazyObject uses synchronization to achieve thread-safety,
+ * guaranteeing that the object will only be created once.
+ * The DoubleCheckImmutableLazyObject also uses synchronization, but
+ * avoids the need for a volatile reference as long as the object being
+ * created is immutable.
+ * All of the LazyObject implementations use an underlying ObjectFactory to
+ * actually create the objects.  This allows us to apply different threading
+ * strategies to a factory.
+ */
+package net.byteseek.object.lazy;
