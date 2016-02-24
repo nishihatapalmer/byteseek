0dec4a637c8e032aaa0185d95f4ec0c252775ea9
Matt Palmer <mattpalms@gmail.com>
2016-02-24 17:42:51 +0000

    Add round trip tests for compilation.
      * A random sequence is generated.
      * A regular expression is taken from the generated sequence.
      * A new sequence is compiled from the generated expression.
      * The two sequences are compared to ensure they match the same bytes at the same positions (even if the classes implementing them are different due to compiler optimisations).

--- src/test/java/net/byteseek/compiler/matcher/SequenceMatcherCompilerTest.java
+++ src/test/java/net/byteseek/compiler/matcher/SequenceMatcherCompilerTest.java
@@ -6,13 +6,12 @@
 package net.byteseek.compiler.matcher;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import net.byteseek.bytes.ByteUtils;
 import net.byteseek.compiler.CompileException;
-import net.byteseek.matcher.bytes.AllBitmaskMatcher;
-import net.byteseek.matcher.bytes.ByteRangeMatcher;
-import net.byteseek.matcher.bytes.OneByteMatcher;
-import net.byteseek.matcher.bytes.SetBinarySearchMatcher;
-import net.byteseek.matcher.bytes.TwoByteMatcher;
+import net.byteseek.matcher.bytes.*;
 import net.byteseek.matcher.sequence.ByteMatcherSequenceMatcher;
 import net.byteseek.matcher.sequence.ByteSequenceMatcher;
 import net.byteseek.matcher.sequence.FixedGapMatcher;
@@ -21,8 +20,11 @@ import net.byteseek.matcher.sequence.SequenceSequenceMatcher;
 import net.byteseek.parser.tree.ParseTree;
 
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.*;
 
 /**
  *
@@ -30,7 +32,9 @@ import org.junit.Test;
  */
 public class SequenceMatcherCompilerTest {
 
-	private SequenceMatcherCompiler	compiler	= new SequenceMatcherCompiler();
+	static Random random = new Random();
+
+	private SequenceMatcherCompiler	compiler;
 
 	/**
 	 * 
@@ -38,20 +42,22 @@ public class SequenceMatcherCompilerTest {
 	public SequenceMatcherCompilerTest() {
 	}
 
-	/**
-	 * 
-	 * @throws Exception
-	 */
 	@BeforeClass
 	public static void setUpClass() throws Exception {
+		final long seed = System.currentTimeMillis();
+		// final long seed = ?
+		random.setSeed(seed);
+		System.out.println("Seeding random number generator with: " + Long.toString(seed));
+		System.out.println("To repeat these exact tests, set the seed to the value above.");
 	}
 
 	/**
 	 * 
 	 * @throws Exception
 	 */
-	@AfterClass
-	public static void tearDownClass() throws Exception {
+	@Before
+	public void setUp() throws Exception {
+		compiler = new SequenceMatcherCompiler();
 	}
 
 	/**
@@ -85,8 +91,6 @@ public class SequenceMatcherCompilerTest {
 	 * Test of compile method, of class SequenceMatcherCompiler.
 	 * @throws Exception 
 	 */
-	//TODO: fix tests:
-
 	@Test
 	public void testBasicCompile() throws Exception {
 		basicTests("00", 1, OneByteMatcher.class);
@@ -130,6 +134,40 @@ public class SequenceMatcherCompilerTest {
 		basicTests(".{1000}", 1000, FixedGapMatcher.class);
 	}
 
+	@Test
+	public void testRandomSequences() throws CompileException {
+		for (int testNo = 0; testNo < 1000; testNo++) {
+			SequenceMatcher generated = createRandomSequenceMatcher();
+			String expression         = generated.toRegularExpression(false);
+			SequenceMatcher compiled = compiler.compile(expression);
+			testSequencesEquivalent(expression, generated, compiled);
+
+			expression = generated.toRegularExpression(true);
+			compiled   = compiler.compile(expression);
+			testSequencesEquivalent(expression, generated, compiled);
+
+			compiled = SequenceMatcherCompiler.compileFrom(expression);
+			testSequencesEquivalent(expression, generated, compiled);
+		}
+	}
+
+	private void testSequencesEquivalent(String expression, SequenceMatcher generated, SequenceMatcher compiled) {
+		assertEquals("Generated and compiled are same length: " + expression, generated.length(), compiled.length());
+		for (int i = 0; i < generated.length(); i++) {
+			ByteMatcher genMatcher = generated.getMatcherForPosition(i);
+			ByteMatcher comMatcher = compiled.getMatcherForPosition(i);
+			String description = "Matcher at position " + i + " in expression: " + expression + " matches same number of bytes";
+			assertEquals(description, genMatcher.getNumberOfMatchingBytes(), comMatcher.getNumberOfMatchingBytes());
+
+			byte[] genBytes = genMatcher.getMatchingBytes();
+			Set<Byte> comBytes = ByteUtils.toSet(comMatcher.getMatchingBytes());
+			assertEquals("byte arrays are same length", genBytes.length, comBytes.size());
+			for (int j = 0; j < genBytes.length; j++) {
+				byte genByte = genBytes[j];
+				assertTrue("Byte " + genByte + " is in compiled version", comBytes.contains(genByte));
+			}
+		}
+	}
 
 	private SequenceMatcher basicTests(String expression, int length, Class<?> matcherClass) {
 		SequenceMatcher matcher = null;
@@ -141,6 +179,82 @@ public class SequenceMatcherCompilerTest {
 			fail(expression + " " + ex.getMessage());
 		}
 		return matcher;
+	}
+
+
+	private SequenceMatcher createRandomSequenceMatcher() {
+		List<SequenceMatcher> matchers = new ArrayList<SequenceMatcher>();
+		int elements = random.nextInt(15) + 1;
+		for (int i = 0; i < elements; i++) {
+			matchers.add(randomSequenceMatcher());
+		}
+		return new SequenceSequenceMatcher(matchers);
+	}
+
+	private ByteMatcher createRandomByteMatcher() {
+		int matcherType = random.nextInt(9);
+		boolean inverted = random.nextBoolean();
+		switch (matcherType) {
+			case 0:
+				return AnyByteMatcher.ANY_BYTE_MATCHER;
+			case 1:
+				return OneByteMatcher.valueOf((byte) random.nextInt(256));
+			case 2:
+				return new InvertedByteMatcher((byte) random.nextInt(256));
+			case 3:
+				return new ByteRangeMatcher(random.nextInt(256), random.nextInt(256), inverted);
+			case 4:
+				return new SetBinarySearchMatcher(createRandomByteSet(), inverted);
+			case 5:
+				return new SetBitsetMatcher(createRandomByteSet(), inverted);
+			case 6:
+				return new TwoByteMatcher((byte) random.nextInt(256), (byte) random.nextInt(256));
+			case 7:
+				return new AllBitmaskMatcher((byte) random.nextInt(256), inverted);
+			case 8:
+				return new AnyBitmaskMatcher((byte) random.nextInt(256), inverted);
+			default:
+				throw new RuntimeException("Case statement doesn't support value " + matcherType);
+		}
+ 	}
+
+	private Set<Byte> createRandomByteSet() {
+		Set<Byte> bytes = new HashSet<Byte>();
+		int numElements = random.nextInt(255) + 1;
+		for (int i = 0; i < numElements; i++) {
+			byte value = (byte) random.nextInt(256);
+			bytes.add(value);
+		}
+		return bytes;
+	}
+
+	private SequenceMatcher randomSequenceMatcher() {
+		int matcherType = random.nextInt(4);
+		switch (matcherType) {
+			case 0: return createRandomByteMatcher();
+			case 1: return new FixedGapMatcher(random.nextInt(10) + 1);
+			case 2: return createRandomByteArrayMatcher();
+			case 3: return createRandomByteMatcherSequence();
+			default: throw new RuntimeException("No such type of matcher defined.");
+		}
+	}
+
+	private ByteSequenceMatcher createRandomByteArrayMatcher() {
+		int length = random.nextInt(15) + 1;
+		byte[] array = new byte[length];
+		for (int i = 0; i < length; i++) {
+			array[i] = (byte) random.nextInt(256);
+		}
+		return new ByteSequenceMatcher(array);
+	}
+
+	private ByteMatcherSequenceMatcher createRandomByteMatcherSequence() {
+		int length = random.nextInt(15) + 1;
+		ByteMatcher[] array = new ByteMatcher[length];
+		for (int i = 0; i < length; i++) {
+			array[i] = createRandomByteMatcher();
+		}
+		return new ByteMatcherSequenceMatcher(array);
 	}
 
 }
