56ae64f685669119f3e793c0071a072589dda8ae
Matt Palmer <mattpalms@gmail.com>
2017-06-10 19:42:46 +0100

    Add processQ2Hash method.

--- src/main/java/net/byteseek/searcher/sequence/AbstractHashSearcher.java
+++ src/main/java/net/byteseek/searcher/sequence/AbstractHashSearcher.java
@@ -135,6 +135,42 @@ public abstract class AbstractHashSearcher extends AbstractFallbackSearcher {
     protected static final OrValue  OR_VALUE  = new OrValue();
     protected static final MakeNegative MAKE_NEGATIVE = new MakeNegative();
 
+    protected int processQ2Hash(final TableStrategy strategy, final int newValue, final int[] SHIFTS,
+                                final int currentHashValue, final int HASH_SHIFT,
+                                final byte[] bytes0, final byte[] bytes1) {
+
+        final int MASK = SHIFTS.length - 1;
+        final long numberOfPermutations = bytes0.length * bytes1.length;
+        final boolean returnHashValue;
+        int hashValue = currentHashValue;
+        if (numberOfPermutations == 1L) { // no permutations to worry about:
+            returnHashValue = true;
+            if (hashValue < 0) { // if we don't have a good last key value, calculate the first 3 elements of it:
+                hashValue = bytes0[0] & 0xFF;
+            }
+            hashValue = (hashValue << HASH_SHIFT) +(bytes1[0] & 0xFF);
+            strategy.processTablePosition(SHIFTS, hashValue & MASK, newValue);
+        } else { // more than one permutation to work through.
+            returnHashValue = false;
+            if (hashValue >= 0) { // Then bytes1 must contain all the additional permutations - just go through them.
+                hashValue = hashValue << HASH_SHIFT;
+                for (final byte permutationValue : bytes1) {
+                    final int permutationHash = hashValue + (permutationValue & 0xFF);
+                    strategy.processTablePosition(SHIFTS, permutationHash & MASK, newValue);
+                }
+
+            } else { // permutations may exist anywhere and in more than one place, use a BytePermutationIterator:
+                final BytePermutationIterator qGramPermutations = new BytePermutationIterator(bytes0, bytes1);
+                while (qGramPermutations.hasNext()) {
+                    // Calculate the hash value:
+                    final byte[] permutationValue = qGramPermutations.next();
+                    hashValue = ((permutationValue[0] & 0xFF) << HASH_SHIFT) + (permutationValue[1] & 0xFF);
+                    strategy.processTablePosition(SHIFTS, hashValue & MASK, newValue);
+                }
+            }
+        }
+        return returnHashValue? hashValue & MASK : -1;
+    }
 
     protected int processQ3Hash(final TableStrategy strategy, final int newValue, final int[] SHIFTS,
                                 final int currentHashValue, final int HASH_SHIFT,
