d32e30c1ca6926301566c498d6a1694e2b93458e
Matt Palmer <mattpalms@gmail.com>
2017-06-12 12:06:05 +0100

    Working version of SignedHash2Searcher using common methods in AbstractQgramSearcher.

--- src/main/java/net/byteseek/searcher/sequence/SignedHash2Searcher.java
+++ src/main/java/net/byteseek/searcher/sequence/SignedHash2Searcher.java
@@ -75,7 +75,7 @@ import java.util.Arrays;
  * ShiftOr creates a table of 256 elements, which in most cases will be the same or smaller
  * than the table used by this searcher, and whose pre-processing time is also faster.
  */
-public final class SignedHash2Searcher extends AbstractHashSearcher {
+public final class SignedHash2Searcher extends AbstractQgramSearcher {
 
     /*************
      * Constants *
@@ -93,13 +93,11 @@ public final class SignedHash2Searcher extends AbstractHashSearcher {
 
     /**
      * A lazy object which can create the information needed to search forwards.
-     * An array of integers is used to determine how far it is safe to shift given a qgram seen in the text.
      */
     private final LazyObject<SearchInfo> forwardSearchInfo;
 
     /**
      * A lazy object which can create the information needed to search backwards.
-     * An array of integers is used to determine how far it is safe to shift given a qgram seen in the text.
      */
     private final LazyObject<SearchInfo> backwardSearchInfo;
 
@@ -241,13 +239,11 @@ public final class SignedHash2Searcher extends AbstractHashSearcher {
         final int MASK = SHIFTS.length - 1;             // SHIFTS is always a power of two in length.
 
         // Determine safe shifts, starts and ends:
-        final int LAST_PATTERN_POS     = localSequence.length() - 1;
-        final int DATA_END_POS         = bytes.length - 1;
-        final int LAST_SEARCH_POS      = toPosition + LAST_PATTERN_POS;
-        final int SEARCH_END           = LAST_SEARCH_POS < DATA_END_POS?
-                                         LAST_SEARCH_POS : DATA_END_POS;
-        final int SEARCH_START         = fromPosition > 0?
-                                         fromPosition : 0;
+        final int LAST_PATTERN_POS = localSequence.length() - 1;
+        final int DATA_END_POS     = bytes.length - 1;
+        final int LAST_SEARCH_POS  = toPosition + LAST_PATTERN_POS;
+        final int SEARCH_END       = LAST_SEARCH_POS < DATA_END_POS? LAST_SEARCH_POS : DATA_END_POS;
+        final int SEARCH_START     = fromPosition > 0? fromPosition : 0;
 
         // Search forwards:
         int searchPos = SEARCH_START + LAST_PATTERN_POS; // look at the end of the pattern to determine shift.
@@ -391,8 +387,7 @@ public final class SignedHash2Searcher extends AbstractHashSearcher {
         final int MASK              = SHIFTS.length - 1; // SHIFTS is always a power of two in length.
 
         // Determine safe shifts, starts and ends:
-        final long SEARCH_END   = toPosition > 0?
-                                  toPosition : 0;
+        final long SEARCH_END = toPosition > 0? toPosition : 0;
 
         // Search forwards:
         Window window;
@@ -533,16 +528,17 @@ public final class SignedHash2Searcher extends AbstractHashSearcher {
             final int[] SHIFTS = new int[TABLE_SIZE];
             Arrays.fill(SHIFTS, MAX_SEARCH_SHIFT);
 
-            // Set up the hash values for hashing as we go along the pattern:
+            // Set up the values for hashing as we go along the pattern:
             byte[] bytes0; // first step of processing shifts all the key values along one, so bytes0 = bytes1, ...
             byte[] bytes1 = localSequence.getMatcherForPosition(qGramStartPos).getMatchingBytes();
 
             // Process all the qgrams in the pattern from the qGram start pos to one before the end of the pattern.
-            int hashValue = 0;
+            int hashValue = -1;
             final int LAST_PATTERN_POS = PATTERN_LENGTH - 1;
             for (int qGramEnd = qGramStartPos + QLEN - 1; qGramEnd < LAST_PATTERN_POS; qGramEnd++) {
-                bytes0 = bytes1; bytes1 = localSequence.getMatcherForPosition(qGramEnd).getMatchingBytes(); // get next byte array.
-                processQ2Hash(SET_VALUE, LAST_PATTERN_POS- qGramEnd, SHIFTS, hashValue, HASH_SHIFT, bytes0, bytes1);
+                bytes0 = bytes1;
+                bytes1 = localSequence.getMatcherForPosition(qGramEnd).getMatchingBytes(); // get next byte array.
+                hashValue = processQ2Hash(SET_VALUE, LAST_PATTERN_POS - qGramEnd, SHIFTS, hashValue, HASH_SHIFT, bytes0, bytes1);
             }
 
             // Make shifts for the last qgrams in the pattern negative:
