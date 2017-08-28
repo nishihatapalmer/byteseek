34aa431526ae51b2ccdfeb078689d257be9c17b8
Matt Palmer <mattpalms@gmail.com>
2017-08-28 11:26:07 +0100

    First (untested) version of new SearchIterator class.
      * Intended to replace the twin SearchForwardIterator and SearchBackwardIterator in a single class.
      * Simpler operation - just instantiate with a Searcher, data source and optionally positions.
      * Safer - can throw IOExceptions during searching - so does not implement the standard Java Iterator interface.
        Previous code swallowed those exceptions, which was not good.

---
+++ src/main/java/net/byteseek/searcher/SearchIterator.java
@@ -0,0 +1,293 @@
+/*
+ * Copyright Matt Palmer 2017, All rights reserved.
+ *
+ * This code is licensed under a standard 3-clause BSD license:
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * The names of its contributors may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.byteseek.searcher;
+
+import net.byteseek.io.reader.WindowReader;
+import net.byteseek.matcher.MatchResult;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ * An iterator over search results, given a Searcher and byte array or WindowReader to search over.
+ * Each iteration returns a list of MatchResults.  Some searchers can return more than one match for a single
+ * position, e.g. if a multi-sequence searcher is used.
+  <p>
+ * Note that the List<MatchResult> returned on each iteration is the same list object each time.
+ * On each iteration the list is cleared and the new results appended.  This avoids creating a new
+ * list for each search iteration.  If you want to retain the MatchResults across iterations,
+ * you should add them to a master list which you seperately maintain.
+ * <p>
+ * Design note: this class does not implement the standard Java Iterator interface.
+ * This is because IOExceptions can occur during searching, and the Iterator interface does not permit
+ * checked exceptions to be thrown. Rather than go around this in some way, we prefer to be familiar but not deceptive.
+ */
+public final class SearchIterator {
+
+    private final BaseSearchIterator iterator;
+
+    private boolean searchedForNext = false;
+    private boolean hasNext;
+
+    /**
+     * Constructs a SearchIterator given a Searcher, a byte array to search in, and the positions to search between.
+     * Searching is forwards unless the from position is bigger than the to position, in which case a backwards
+     * search will be performed.
+     * Searching will only take place within the bounds of the data, so it is safe to specify any values for the positions.
+     *
+     * @param searcher The Searcher to search with.
+     * @param data A byte array which is the data to search in.
+     * @param from The position to start searching from.  If bigger than the to position, searching will be backwards.
+     * @param to The position to finish searching at.
+     */
+    public SearchIterator(final Searcher searcher, final byte[] data, final int from, final int to) {
+        iterator = from <= to? new byteForwardIterator(searcher, data, from, to)
+                             : new byteBackwardIterator(searcher, data, from, to);
+    }
+
+    /*
+     * Constructs a SearchIterator given a Searcher, a WindowReader to search in, and the positions to search between.
+     * Searching is forwards unless the from position is bigger than the to position, in which case a backwards
+     * search will be performed.
+     * Searching will only take place within the bounds of the data, so it is safe to specify any values for the positions.
+     *
+     * @param searcher The Searcher to search with.
+     * @param reader A WindowReader which is the data to search in.
+     * @param from The position to start searching from.  If bigger than the to position, searching will be backwards.
+     * @param to The position to finish searching at.
+     */
+    public SearchIterator(final Searcher searcher, final WindowReader reader, final long from, final long to) {
+        iterator = from <= to? new readerForwardIterator(searcher, reader, from, to)
+                             : new readerBackwardIterator(searcher, reader, from, to);
+    }
+
+    /**
+     * Returns true if there are more search results available.
+     *
+     * @return true if there are more search results available.
+     * @throws IOException If there was a problem reading the search data.
+     */
+    public boolean hasNext() throws IOException {
+        if (!searchedForNext) {
+            hasNext = iterator.hasNext();
+            searchedForNext = true;
+        }
+        return hasNext;
+    }
+
+    /**
+     * Returns a List of MatchResults containing the next set of results.
+     * Note that this implementation returns the same List each time next is called, clearing any previous results.
+     * This is to avoid creating a new list on each search iteration.  If you want to retain all the MatchResults,
+     * across iterations, you should add them to a Collection which you maintain separately.
+     *
+     * @return a List of MatchResults containing the next set of results.
+     * @throws IOException If there was a problem reading the search data.
+     * @throws NoSuchElementException if next() is called but there are no further results.
+     */
+    public List<MatchResult> next() throws IOException {
+        if (hasNext()) {
+            searchedForNext = false;
+            return iterator.next();
+        }
+        throw new NoSuchElementException("No more results for search iterator: " + iterator);
+    }
+
+    /**
+     * An abstract base class for all search iterators, implementing most of the logic.
+     * Subclasses are the different search strategies - e.g. forwards in a byte array, backwards in a reader, etc.
+     */
+    private static abstract class BaseSearchIterator {
+
+        protected final Searcher searcher;
+        protected final long to;
+        protected final List<MatchResult> results = new ArrayList<MatchResult>();
+
+        protected long pos;
+
+        private BaseSearchIterator(final Searcher searcher, final long from, final long to) {
+            this.searcher      = searcher;
+            this.pos           = from;
+            this.to            = to;
+        }
+
+        /**
+         * Clears any previous results, searches for the next set of results, adding them to the results.
+         * Returns true if there were any results.
+         *
+         * @return true if there were any results.
+         * @throws IOException If there was a problem reading the search data.
+         */
+        private boolean hasNext() throws IOException {
+            results.clear();
+            return getNumNextResults() > 0;
+        }
+
+        /**
+         * Sets the search position to the next place to search from and returns the current set of results.
+         * @return The current set of results.
+         */
+        private List<MatchResult> next() {
+            pos = nextSearchPosition();
+            return results;
+        }
+
+        /**
+         * Searches in the data for the next set of results, placing them in the results List,
+         * and returning the number of results added.
+         *
+         * @return The number of results added.
+         * @throws IOException If there was a problem reading the search data.
+         */
+        protected abstract int getNumNextResults() throws IOException;
+
+        /**
+         * Returns the next valid search position for the iterator.
+         * @return the next valid search position for the iterator.
+         */
+        protected abstract long nextSearchPosition();
+
+        /**
+         * Returns the search position one on from the furthest match we have when searching forwards.
+         *
+         * @return the search position one on from the furthest match we have when searching forwards.
+         */
+        protected long getNextForwardSearchPosition() {
+            long furthestPosition = Long.MIN_VALUE;
+            for (final MatchResult result : results) {
+                final long resultPosition = result.getMatchPosition();
+                if (resultPosition > furthestPosition) {
+                    furthestPosition = resultPosition;
+                }
+            }
+            return furthestPosition + 1;
+        }
+
+        /**
+         * Returns the search position one behind the further match we have when searching backwards.
+         *
+         * @return the search position one behind the further match we have when searching backwards.
+         */
+        protected long getNextBackwardSearchPosition() {
+            long furthestPosition = Long.MAX_VALUE;
+            for (final MatchResult result : results) {
+                final long resultPosition = result.getMatchPosition();
+                if (resultPosition < furthestPosition) {
+                    furthestPosition = resultPosition;
+                }
+            }
+            return furthestPosition - 1;
+        }
+
+        @Override
+        public String toString() {
+            return getClass().getSimpleName() + "(searcher:" + searcher + " pos: " + pos + " to:" + to + ')';
+        }
+    }
+
+    /**
+     * Searches forwards over a byte array.
+     */
+    private final class byteForwardIterator extends BaseSearchIterator {
+        private final byte[] data;
+        private byteForwardIterator(final Searcher searcher, final byte[] data, final int from, final int to) {
+            super(searcher, from, to);
+            this.data = data;
+        }
+        @Override
+        protected int getNumNextResults() {
+            return searcher.searchForwards(data, (int) pos, (int) to, results);
+        }
+        @Override
+        protected long nextSearchPosition() {
+            return getNextForwardSearchPosition();
+        }
+    }
+
+    /**
+     * Searches backwards over a byte array.
+     */
+    private final class byteBackwardIterator extends BaseSearchIterator {
+        private final byte[] data;
+        private byteBackwardIterator(final Searcher searcher, final byte[] data, final int from, final int to) {
+            super(searcher, from, to);
+            this.data = data;
+        }
+        @Override
+        protected int getNumNextResults() {
+            return searcher.searchBackwards(data, (int) pos, (int) to, results);
+        }
+        @Override
+        protected long nextSearchPosition() {
+            return getNextBackwardSearchPosition();
+        }
+    }
+
+    /**
+     * Searches forwards in a WindowReader.
+     */
+    private final class readerForwardIterator extends BaseSearchIterator {
+        private final WindowReader reader;
+        private readerForwardIterator(final Searcher searcher, final WindowReader reader, final long from, final long to) {
+            super(searcher, from, to);
+            this.reader = reader;
+        }
+        @Override
+        protected int getNumNextResults() throws IOException {
+            return searcher.searchForwards(reader, pos, to, results);
+        }
+        @Override
+        protected long nextSearchPosition() {
+            return getNextForwardSearchPosition();
+        }
+    }
+
+    /**
+     * Searches backwards in a WindowReader.
+     */
+    private final class readerBackwardIterator extends BaseSearchIterator {
+        private final WindowReader reader;
+        private readerBackwardIterator(final Searcher searcher, final WindowReader reader, final long from, final long to) {
+            super(searcher, from, to);
+            this.reader = reader;
+        }
+        @Override
+        protected int getNumNextResults() throws IOException {
+            return searcher.searchBackwards(reader, pos, to, results);
+        }
+        @Override
+        protected long nextSearchPosition() {
+            return getNextBackwardSearchPosition();
+        }
+    }
+}
