70177febe726faf92dce8368d826adf94e10b27f
Matt Palmer <mattpalms@gmail.com>
2017-12-10 14:42:02 +0000

    Add abstract superclass and package javadoc.

---
+++ src/main/java/net/byteseek/searcher/sequence/factory/AbstractSequenceFactory.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright Matt Palmer 2017, All rights reserved.
+ *
+ * This code is licensed under a standard 3-clause BSD license:
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * The names of its contributors may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ */
+package net.byteseek.searcher.sequence.factory;
+
+import net.byteseek.compiler.CompileException;
+import net.byteseek.compiler.matcher.SequenceMatcherCompiler;
+import net.byteseek.matcher.bytes.ByteMatcher;
+import net.byteseek.matcher.sequence.ByteSequenceMatcher;
+import net.byteseek.matcher.sequence.SequenceMatcher;
+import net.byteseek.searcher.bytes.ByteMatcherSearcher;
+import net.byteseek.searcher.bytes.ByteSearcher;
+import net.byteseek.searcher.sequence.SequenceSearcher;
+import net.byteseek.searcher.sequence.ShiftOrUnrolledSearcher;
+import net.byteseek.searcher.sequence.SignedHash2Searcher;
+import net.byteseek.utils.ArgUtils;
+
+/**
+ * A SequenceSearcherFactory that implements all the "no-brainer" decisions on what Searcher to use - for
+ * anything that only has a length of one.  In these cases, there isn't much competition - a simple dedicated
+ * searcher for them will outperform anything more complex - and use less memory too.
+ * <p>
+ * Subclasses must implement the createSequenceSearcher(SequenceMatcher theMatcher) method, to create an appropriate
+ * sequence searcher for any sequence longer than one.
+ */
+public abstract class AbstractSequenceFactory implements SequenceSearcherFactory {
+
+    @Override
+    public SequenceSearcher create(final byte theByte) {
+        return new ByteSearcher(theByte);
+    }
+
+    @Override
+    public SequenceSearcher create(final byte[] theBytes) {
+        ArgUtils.checkNullOrEmptyByteArray(theBytes, "theBytes");
+        if (theBytes.length == 1) {
+            return create(theBytes[0]);
+        }
+        return create(new ByteSequenceMatcher(theBytes));
+    }
+
+    @Override
+    public SequenceSearcher create(final String regex) throws CompileException {
+        ArgUtils.checkNullOrEmptyString(regex, "regex");
+        return create(SequenceMatcherCompiler.compileFrom(regex));
+    }
+
+    @Override
+    public SequenceSearcher create(final ByteMatcher theMatcher) {
+        ArgUtils.checkNullObject(theMatcher, "theMatcher");
+        if (theMatcher.getNumberOfMatchingBytes() == 1) {
+            return create(theMatcher.getMatchingBytes()[0]);
+        }
+        return new ByteMatcherSearcher(theMatcher);
+    }
+
+    @Override
+    public SequenceSearcher create(final SequenceMatcher theSequence) {
+        ArgUtils.checkNullObject(theSequence, "theSequence");
+        if (theSequence.length() == 1) {
+            return create(theSequence.getMatcherForPosition(0));
+        }
+        return createSequenceSearcher(theSequence);
+    }
+
+    /**
+     * Create a searcher for a sequence greater than one in length.
+     *
+     * @param theSequence The sequence greater than one in length.
+     * @return A SequenceSearcher for that sequence.
+     */
+    protected abstract SequenceSearcher createSequenceSearcher(SequenceMatcher theSequence);
+}
---
+++ src/main/java/net/byteseek/searcher/sequence/factory/package-info.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright Matt Palmer 2017, All rights reserved.
+ *
+ * This code is licensed under a standard 3-clause BSD license:
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * The names of its contributors may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * A package containing factories for SequenceSearchers.  There is rarely a single searcher that
+ * works well for all sequences.
+ * <p>
+ * When the sequence length is just one, there are searchers that will always outperform any other.
+ * The AbstractSequenceFactory implements all of those possibilities, leaving subclasses to use other
+ * strategies for sequences with a length greater than one.
+ * <p>
+ * For short sequences it is hard to beat the ShiftOr searchers.  Even though these examine every byte
+ * and do not attempt to skip, they use bit-parallelism to verify a match (essentially for free), they
+ * are simple, and they have good cache locality.
+ * <p>
+ * Longer sequences benefit from approaches which allow them to skip ahead in the search data, based on
+ * various ways of indexing the patterns they are searching for.  This skip is always limited by the maximum
+ * length of the pattern to be searched for.  They can never skip more than this ahead, or they risk missing
+ * a match.  Therefore, longer sequences allow for longer skips ahead.
+ * <p>
+ * However, there is not any single searcher for longer sequences which is better than all others in all
+ * circumstances.  Some perform very well but have expensive worst cases.  For example, in QGramFiltering where
+ * either the index table fills up, or the pattern and data have a very low alphabet (e.g. DNA), this algorithm
+ * performs extremely poorly - far worse than just doing a naive search - but is very fast otherwise.
+ * Some generally perform well, but may not be the fastest for all searches (e.g. SignedHash, SignedHorpsool, UnrolledHorspool)
+ */
+package net.byteseek.searcher.sequence.factory;
