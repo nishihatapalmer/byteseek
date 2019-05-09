b4caf91d17b1d45599f4e11efff71732ac232fa7
Matt Palmer <mattpalms@gmail.com>
2019-05-09 10:58:01 +0100

    Add tests for CompileException.

---
+++ src/test/java/net/byteseek/compiler/CompileExceptionTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright Matt Palmer 2019, All rights reserved.
+ *
+ * This code is licensed under a standard 3-clause BSD license:
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * The names of its contributors may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.byteseek.compiler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CompileExceptionTest {
+
+    @Test
+    public void constructMessage() {
+        String message = "the message";
+        CompileException exception = new CompileException(message);
+        assertTrue(exception.getMessage().contains("message"));
+        assertNull(exception.getCause());
+    }
+
+    @Test
+    public void constructThrowable() {
+        Throwable cause = new RuntimeException();
+        CompileException exception = new CompileException(cause);
+        assertEquals(cause, exception.getCause());
+    }
+
+    @Test
+    public void constructThrowableMessage() {
+        String message = "the message";
+        Throwable cause = new RuntimeException();
+        CompileException exception = new CompileException(message, cause);
+        assertTrue(exception.getMessage().contains("message"));
+        assertEquals(cause, exception.getCause());
+    }
+
+}
