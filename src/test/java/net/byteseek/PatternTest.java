bb7c922a58bcded1c5459cf26d857e27d4aed0e7
matt <mattpalms@gmail.com>
2020-12-10 21:49:32 +0000

    Refactor to Pattern.

---
+++ src/test/java/net/byteseek/PatternTest.java
@@ -0,0 +1,129 @@
+package net.byteseek;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PatternTest {
+
+    @Test
+    public void matches() {
+        fail("TODO");
+    }
+
+    @Test
+    public void testMatches() {
+    }
+
+    @Test
+    public void testMatches1() {
+    }
+
+    @Test
+    public void testMatches2() {
+    }
+
+    @Test
+    public void searchForwards() {
+    }
+
+    @Test
+    public void testSearchForwards() {
+    }
+
+    @Test
+    public void testSearchForwards1() {
+    }
+
+    @Test
+    public void testSearchForwards2() {
+    }
+
+    @Test
+    public void testSearchForwards3() {
+    }
+
+    @Test
+    public void testSearchForwards4() {
+    }
+
+    @Test
+    public void testSearchForwards5() {
+    }
+
+    @Test
+    public void testSearchForwards6() {
+    }
+
+    @Test
+    public void testSearchForwards7() {
+    }
+
+    @Test
+    public void testSearchForwards8() {
+    }
+
+    @Test
+    public void testSearchForwards9() {
+    }
+
+    @Test
+    public void testSearchForwards10() {
+    }
+
+    @Test
+    public void searchBackwards() {
+    }
+
+    @Test
+    public void testSearchBackwards() {
+    }
+
+    @Test
+    public void testSearchBackwards1() {
+    }
+
+    @Test
+    public void testSearchBackwards2() {
+    }
+
+    @Test
+    public void testSearchBackwards3() {
+    }
+
+    @Test
+    public void testSearchBackwards4() {
+    }
+
+    @Test
+    public void testSearchBackwards5() {
+    }
+
+    @Test
+    public void testSearchBackwards6() {
+    }
+
+    @Test
+    public void testSearchBackwards7() {
+    }
+
+    @Test
+    public void testSearchBackwards8() {
+    }
+
+    @Test
+    public void testSearchBackwards9() {
+    }
+
+    @Test
+    public void testSearchBackwards10() {
+    }
+
+    @Test
+    public void expression() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}
